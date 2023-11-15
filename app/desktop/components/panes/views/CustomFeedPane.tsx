6e4daf200e5ff2c98e46f6217a04ee0f54f7836e
Mary <pineapplecreamcheese@skiff.com>
2023-11-15 10:25:47 +0700

    feat: pane settings on all panes

--- app/desktop/components/panes/views/CustomFeedPane.tsx
+++ app/desktop/components/panes/views/CustomFeedPane.tsx
@@ -1,3 +1,5 @@
+import { createSignal } from 'solid-js';
+
 import type { CustomFeedPaneConfig } from '../../../globals/panes.ts';
 
 import iconButton from '~/com/primitives/icon-button.ts';
@@ -8,24 +10,42 @@ import SettingsIcon from '~/com/icons/baseline-settings.tsx';
 
 import { usePaneContext } from '../PaneContext.tsx';
 import Pane from '../Pane.tsx';
+import PaneAside from '../PaneAside.tsx';
 import PaneBody from '../PaneBody.tsx';
 import PaneHeader from '../PaneHeader.tsx';
 
+import DeletePaneSettings from '../settings/DeletePaneSettings.tsx';
+import PaneSizeSettings from '../settings/PaneSizeSettings.tsx';
+
 const CustomFeedPane = () => {
+	const [isSettingsOpen, setIsSettingsOpen] = createSignal(false);
+
 	const { pane } = usePaneContext<CustomFeedPaneConfig>();
 
 	return (
-		<Pane>
-			<PaneHeader title={pane.feed.name} subtitle="Feed">
-				<button class={/* @once */ iconButton({ edge: 'right', color: 'muted' })}>
-					<SettingsIcon class="place-self-center" />
-				</button>
-			</PaneHeader>
+		<>
+			<Pane>
+				<PaneHeader title={pane.feed.name} subtitle="Feed">
+					<button
+						onClick={() => setIsSettingsOpen(!isSettingsOpen())}
+						class={/* @once */ iconButton({ edge: 'right', color: 'muted' })}
+					>
+						<SettingsIcon class="place-self-center" />
+					</button>
+				</PaneHeader>
 
-			<PaneBody>
-				<TimelineList uid={pane.uid} params={{ type: 'feed', uri: pane.feed.uri }} />
-			</PaneBody>
-		</Pane>
+				<PaneBody>
+					<TimelineList uid={pane.uid} params={{ type: 'feed', uri: pane.feed.uri }} />
+				</PaneBody>
+			</Pane>
+
+			{isSettingsOpen() && (
+				<PaneAside>
+					<PaneSizeSettings />
+					<DeletePaneSettings />
+				</PaneAside>
+			)}
+		</>
 	);
 };
 
--- app/desktop/components/panes/views/CustomListPane.tsx
+++ app/desktop/components/panes/views/CustomListPane.tsx
@@ -1,3 +1,5 @@
+import { createSignal } from 'solid-js';
+
 import type { CustomListPaneConfig } from '../../../globals/panes.ts';
 
 import iconButton from '~/com/primitives/icon-button.ts';
@@ -8,24 +10,42 @@ import SettingsIcon from '~/com/icons/baseline-settings.tsx';
 
 import { usePaneContext } from '../PaneContext.tsx';
 import Pane from '../Pane.tsx';
+import PaneAside from '../PaneAside.tsx';
 import PaneBody from '../PaneBody.tsx';
 import PaneHeader from '../PaneHeader.tsx';
 
+import DeletePaneSettings from '../settings/DeletePaneSettings.tsx';
+import PaneSizeSettings from '../settings/PaneSizeSettings.tsx';
+
 const CustomListPane = () => {
+	const [isSettingsOpen, setIsSettingsOpen] = createSignal(false);
+
 	const { pane } = usePaneContext<CustomListPaneConfig>();
 
 	return (
-		<Pane>
-			<PaneHeader title={pane.list.name} subtitle="User list">
-				<button class={/* @once */ iconButton({ edge: 'right', color: 'muted' })}>
-					<SettingsIcon class="place-self-center" />
-				</button>
-			</PaneHeader>
+		<>
+			<Pane>
+				<PaneHeader title={pane.list.name} subtitle="User list">
+					<button
+						onClick={() => setIsSettingsOpen(!isSettingsOpen())}
+						class={/* @once */ iconButton({ edge: 'right', color: 'muted' })}
+					>
+						<SettingsIcon class="place-self-center" />
+					</button>
+				</PaneHeader>
 
-			<PaneBody>
-				<TimelineList uid={pane.uid} params={{ type: 'list', uri: pane.list.uri }} />
-			</PaneBody>
-		</Pane>
+				<PaneBody>
+					<TimelineList uid={pane.uid} params={{ type: 'list', uri: pane.list.uri }} />
+				</PaneBody>
+			</Pane>
+
+			{isSettingsOpen() && (
+				<PaneAside onClose={() => setIsSettingsOpen(false)}>
+					<PaneSizeSettings />
+					<DeletePaneSettings />
+				</PaneAside>
+			)}
+		</>
 	);
 };
 
