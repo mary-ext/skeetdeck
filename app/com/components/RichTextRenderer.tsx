2c3c59fbb6b2842f4cb2cd19629cefb922e53d05
Mary <pineapplecreamcheese@skiff.com>
2023-11-15 10:09:46 +0700

    refactor: move toShortUrl and isLinkValid

--- app/com/components/RichTextRenderer.tsx
+++ app/com/components/RichTextRenderer.tsx
@@ -1,7 +1,9 @@
 import { type JSX } from 'solid-js';
-import { segmentRichText } from '~/api/richtext/segmentize.ts';
 
+import { isLinkValid } from '~/api/richtext/renderer.ts';
+import { segmentRichText } from '~/api/richtext/segmentize.ts';
 import type { Facet, RichTextSegment } from '~/api/richtext/types.ts';
+
 import { type Linking, LinkingType, useLinking } from './Link.tsx';
 
 export interface RichTextItem {
@@ -122,74 +124,4 @@ const renderRichText = (segments: RichTextSegment[]): RichTextUiSegment[] => {
 	}
 
 	return ui;
-};
-
-const TRIM_HOST_RE = /^www\./;
-const TRIM_URLTEXT_RE = /^\s*(https?:\/\/)?(?:www\.)?/;
-const PATH_MAX_LENGTH = 18;
-
-export const toShortUrl = (uri: string): string => {
-	try {
-		const url = new URL(uri);
-		const protocol = url.protocol;
-
-		const host = url.host.replace(TRIM_HOST_RE, '');
-		const pathname = url.pathname;
-
-		const path = (pathname === '/' ? '' : pathname) + url.search + url.hash;
-
-		if (protocol === 'http:' || protocol === 'https:') {
-			if (path.length > PATH_MAX_LENGTH) {
-				return host + path.slice(0, PATH_MAX_LENGTH - 1) + '…';
-			}
-
-			return host + path;
-		}
-	} catch {}
-
-	return uri;
-};
-
-const buildHostPart = (url: URL) => {
-	const username = url.username;
-	// const password = url.password;
-
-	const hostname = url.hostname.replace(TRIM_HOST_RE, '').toLowerCase();
-	const port = url.port;
-
-	// const auth = username ? username + (password ? ':' + password : '') + '@' : '';
-
-	// Perhaps might be best if we always warn on authentication being passed.
-	const auth = username ? '\0@@\0' : '';
-	const host = hostname + (port ? ':' + port : '');
-
-	return auth + host;
-};
-
-export const isLinkValid = (uri: string, text: string) => {
-	let url: URL;
-	let protocol: string;
-	try {
-		url = new URL(uri);
-		protocol = url.protocol;
-
-		if (protocol !== 'https:' && protocol !== 'http:') {
-			return false;
-		}
-	} catch {
-		return false;
-	}
-
-	const expectedHost = buildHostPart(url);
-	const length = expectedHost.length;
-
-	const normalized = text.replace(TRIM_URLTEXT_RE, '').toLowerCase();
-	const normalizedLength = normalized.length;
-
-	const boundary = normalizedLength >= length ? normalized[length] : undefined;
-
-	return (
-		(!boundary || boundary === '/' || boundary === '?' || boundary === '#') &&
-		normalized.startsWith(expectedHost)
-	);
 };
