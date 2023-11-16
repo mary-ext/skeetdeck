4ed826ad945086cffde72b04d7058bb7eaf358c1
Mary <pineapplecreamcheese@skiff.com>
2023-11-16 16:31:54 +0700

    feat: rename search column

--- app/desktop/components/panes/settings/SearchPaneSettings.tsx
+++ app/desktop/components/panes/settings/SearchPaneSettings.tsx
@@ -1,8 +1,13 @@
+import { Show } from 'solid-js';
+
 import TextareaAutoresize from 'solid-textarea-autosize';
 
 import type { SearchPaneConfig } from '../../../globals/panes.ts';
 
+import { Input } from '~/com/primitives/input.ts';
 import { Textarea } from '~/com/primitives/textarea.ts';
+
+import Checkbox from '~/com/components/inputs/Checkbox.tsx';
 
 import SearchIcon from '~/com/icons/baseline-search.tsx';
 
@@ -38,6 +43,40 @@ const SearchPaneSettings = () => {
 						}}
 					/>
 				</div>
+			</div>
+
+			<div class="border-b border-divider">
+				<label class="flex items-center justify-between gap-4 p-4">
+					<span class="text-sm">Rename column</span>
+					<Checkbox
+						checked={pane.title !== null}
+						onChange={(ev) => {
+							pane.title = !ev.currentTarget.checked ? null : '';
+						}}
+					/>
+				</label>
+
+				<Show when={pane.title !== null}>
+					<div class="p-4 pt-0">
+						<input
+							class={/* @once */ Input()}
+							value={pane.title!}
+							onKeyDown={(ev) => {
+								if (ev.key === 'Enter') {
+									const value = ev.currentTarget.value.trim();
+
+									if (value) {
+										pane.title = value;
+									} else {
+										ev.currentTarget.value = pane.title!;
+									}
+
+									ev.preventDefault();
+								}
+							}}
+						/>
+					</div>
+				</Show>
 			</div>
 		</div>
 	);
