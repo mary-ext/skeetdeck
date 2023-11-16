2bca4a9e13199345c64b085c23c5483728687dc1
Mary <pineapplecreamcheese@skiff.com>
2023-11-16 10:26:59 +0700

    feat: add batched querymeta for later usage

--- app/api/api-env.ts
+++ app/api/api-env.ts
@@ -6,6 +6,7 @@ import type { FilterPreferences, LanguagePreferences } from './types.ts';
 declare module '@pkg/solid-query' {
 	interface Register {
 		queryMeta: {
+			batched?: boolean;
 			timelineOpts?: {
 				moderation: ModerationOpts;
 				filters: FilterPreferences;
