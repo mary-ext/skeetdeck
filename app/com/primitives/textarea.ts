f03357c30adedadf8b0014afca549d67dde0d30d
Mary <pineapplecreamcheese@skiff.com>
2023-11-16 16:13:27 +0700

    feat: edit search pane

--- app/com/primitives/textarea.ts
+++ app/com/primitives/textarea.ts
@@ -5,7 +5,7 @@ export interface TextareaProps {
 export const Textarea = (props: TextareaProps = {}) => {
 	const { class: className } = props;
 
-	let cn = `block w-full resize-none rounded-md border border-input bg-transparent px-3 py-2 text-sm outline-2 -outline-offset-1 outline-accent outline-none placeholder:text-muted-fg focus:outline disabled:opacity-50`;
+	let cn = `block w-full resize-none rounded-md border border-input bg-transparent px-3 py-[0.45rem] text-sm outline-2 -outline-offset-1 outline-accent outline-none placeholder:text-muted-fg focus:outline disabled:opacity-50`;
 
 	if (className) {
 		return `${cn} ${className}`;
--- app/desktop/components/panes/settings/SearchPaneSettings.tsx
+++ app/desktop/components/panes/settings/SearchPaneSettings.tsx
@@ -1,7 +1,44 @@
+import TextareaAutoresize from 'solid-textarea-autosize';
+
+import type { SearchPaneConfig } from '../../../globals/panes.ts';
+
+import { Textarea } from '~/com/primitives/textarea.ts';
+
+import SearchIcon from '~/com/icons/baseline-search.tsx';
+
+import { usePaneContext } from '../PaneContext.tsx';
+
 const SearchPaneSettings = () => {
+	const { pane } = usePaneContext<SearchPaneConfig>();
+
 	return (
 		<div class="contents">
-			<div class="border-b border-divider p-4"></div>
+			<div class="border-b border-divider p-4">
+				<div class="relative grow">
+					<div class="pointer-events-none absolute inset-y-0 ml-px grid place-items-center px-2">
+						<SearchIcon class="text-lg text-muted-fg" />
+					</div>
+					<TextareaAutoresize
+						class={/* @once */ Textarea({ class: 'pl-8' })}
+						value={pane.query}
+						placeholder="Search..."
+						minRows={1}
+						onKeyDown={(ev) => {
+							if (ev.key === 'Enter') {
+								const value = ev.currentTarget.value.trim();
+
+								if (value) {
+									pane.query = value;
+								} else {
+									ev.currentTarget.value = pane.query;
+								}
+
+								ev.preventDefault();
+							}
+						}}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
