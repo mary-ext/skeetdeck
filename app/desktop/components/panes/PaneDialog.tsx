dc6b0a18dcf340e7c202b17a3c8351f587461a8a
Mary <pineapplecreamcheese@skiff.com>
2023-11-17 10:58:38 +0700

    fix: cap pane modal width

--- app/desktop/components/panes/PaneDialog.tsx
+++ app/desktop/components/panes/PaneDialog.tsx
@@ -1,11 +1,39 @@
-import type { JSX } from 'solid-js';
+import { createMemo, type JSX } from 'solid-js';
+
+import { preferences } from '../../globals/settings.ts';
+import { PaneSize, SpecificPaneSize } from '../../globals/panes.ts';
+
+import { usePaneContext } from './PaneContext.tsx';
 
 export interface PaneDialogProps {
 	children?: JSX.Element;
 }
 
 const PaneDialog = (props: PaneDialogProps) => {
-	return <div class="flex h-full flex-col bg-background">{props.children}</div>;
+	const { pane } = usePaneContext();
+
+	const size = createMemo(() => {
+		const $size = pane.size;
+
+		if ($size === SpecificPaneSize.INHERIT) {
+			return preferences.ui.defaultPaneSize;
+		}
+
+		return $size;
+	});
+
+	return (
+		<div
+			class="flex h-full flex-col bg-background"
+			classList={{
+				[`w-84`]: size() === PaneSize.SMALL,
+				[`w-96`]: size() === PaneSize.MEDIUM,
+				[`w-120`]: size() === PaneSize.LARGE,
+			}}
+		>
+			{props.children}
+		</div>
+	);
 };
 
 export default PaneDialog;
