7581df32e5efb054d55ac93022c777f191353fbe
Mary <pineapplecreamcheese@skiff.com>
2023-11-19 17:09:32 +0700

    refactor: clean up createMutation

--- packages/solid-query/lib/createMutation.ts
+++ packages/solid-query/lib/createMutation.ts
@@ -1,12 +1,11 @@
 import { type DefaultError, MutationObserver } from '@tanstack/query-core';
 
-import { createComputed, on, onCleanup } from 'solid-js';
+import { createRenderEffect, on, onCleanup, untrack } from 'solid-js';
 import { createStore } from 'solid-js/store';
 
 import type { QueryClient } from './QueryClient.ts';
 import { useQueryClient } from './QueryClientProvider.tsx';
 import type { CreateMutateFunction, CreateMutationOptions, CreateMutationResult } from './types.ts';
-import { shouldThrowError } from './utils.ts';
 
 // HOOK
 export function createMutation<TData = unknown, TError = DefaultError, TVariables = void, TContext = unknown>(
@@ -15,7 +14,7 @@ export function createMutation<TData = unknown, TError = DefaultError, TVariable
 ): CreateMutationResult<TData, TError, TVariables, TContext> {
 	const client = useQueryClient(queryClient);
 
-	const observer = new MutationObserver<TData, TError, TVariables, TContext>(client, options());
+	const observer = new MutationObserver<TData, TError, TVariables, TContext>(client, untrack(options));
 
 	const mutate: CreateMutateFunction<TData, TError, TVariables, TContext> = (variables, mutateOptions) => {
 		observer.mutate(variables, mutateOptions).catch(noop);
@@ -27,33 +26,27 @@ export function createMutation<TData = unknown, TError = DefaultError, TVariable
 		mutateAsync: observer.getCurrentResult().mutate,
 	});
 
-	createComputed(() => {
-		observer.setOptions(options());
-	});
-
-	createComputed(
+	createRenderEffect(
 		on(
-			() => state.status,
-			() => {
-				if (state.isError && shouldThrowError(observer.options.throwOnError, [state.error])) {
-					throw state.error;
-				}
+			options,
+			($options) => {
+				observer.setOptions($options);
 			},
+			{ defer: true },
 		),
 	);
 
-	const unsubscribe = observer.subscribe((result) => {
-		setState({
-			...result,
-			mutate,
-			mutateAsync: result.mutate,
-		});
-	});
-
-	onCleanup(unsubscribe);
+	onCleanup(
+		observer.subscribe((result) => {
+			setState({
+				...result,
+				mutate,
+				mutateAsync: result.mutate,
+			});
+		}),
+	);
 
 	return state;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
 function noop() {}
