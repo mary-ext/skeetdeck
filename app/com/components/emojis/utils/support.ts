eafcf80630e9cf7c5e1e8717c5cd222d68ecbd84
Mary <pineapplecreamcheese@skiff.com>
2023-11-22 16:09:01 +0700

    refactor: simplify canvas check

--- app/com/components/emojis/utils/support.ts
+++ app/com/components/emojis/utils/support.ts
@@ -9,11 +9,6 @@ import { scheduleIdleTask } from '~/utils/idle.ts';
 // https://github.com/nolanlawson/emoji-picker-element/pull/268#issuecomment-1073347283
 const FONT_FAMILY = '"Twemoji Mozilla", "Noto Color Emoji", sans-serif';
 
-const CANVAS_HEIGHT = 25;
-const CANVAS_WIDTH = 20;
-
-const FONT_SIZE = Math.floor(CANVAS_HEIGHT / 2);
-
 // Find one good representative emoji from each version to test by checking its color.
 // Ideally it should have color in the center. For some inspiration, see:
 // https://about.gitlab.com/blog/2018/05/30/journey-in-native-unicode-emoji/
@@ -38,152 +33,37 @@ const emojiVersions: [emoji: string, version: number][] = [
 	['😃', 0.6],
 ];
 
-// Taken from https://github.com/koala-interactive/is-emoji-supported
-// Licensed under MIT license
-
 // Unfortunately, this code doesn't handle compound emojis, which is necessary
 // for checking if the system actually supports Unicode 12.1 and 13.1, so let's
 // pretend that if Unicode 12 is supported, 12.1 is supported as well.
+const getTextFeature = (text: string, color: string) => {
+	const canvas = document.createElement('canvas');
+	canvas.width = canvas.height = 1;
 
-// let ctx: CanvasRenderingContext2D | null | undefined;
-
-// export const isEmojiSupportedUncached = (emoji: string): boolean => {
-// 	if (ctx === undefined) {
-// 		try {
-// 			ctx = document.createElement('canvas').getContext('2d')!;
-// 		} catch {}
-
-// 		// Set the canvas up for reuse
-// 		if (ctx) {
-// 			ctx.font = `${FONT_SIZE}px ${FONT_FAMILY}`;
-// 			ctx.textBaseline = 'top';
-// 			ctx.canvas.width = CANVAS_WIDTH * 2;
-// 			ctx.canvas.height = CANVAS_HEIGHT;
-// 		}
-// 	}
-
-// 	// If canvas is blocked, we'll take tofu boxes over filtering everything out
-// 	if (ctx == null) {
-// 		return true;
-// 	}
-
-// 	ctx.clearRect(0, 0, CANVAS_WIDTH * 2, CANVAS_HEIGHT);
-
-// 	// Draw in red on the left
-// 	ctx.fillStyle = '#FF0000';
-// 	ctx.fillText(emoji, 0, 22);
-
-// 	// Draw in blue on right
-// 	ctx.fillStyle = '#0000FF';
-// 	ctx.fillText(emoji, CANVAS_WIDTH, 22);
-
-// 	const a = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT).data;
-// 	const count = a.length;
-// 	let i = 0;
-
-// 	// Search the first visible pixel
-// 	for (; i < count && !a[i + 3]; i += 4);
-
-// 	// No visible pixel
-// 	if (i >= count) {
-// 		return false;
-// 	}
-
-// 	// Emoji has immutable color, so we check the color of the emoji in two different colors
-// 	// the result show be the same.
-// 	const x = CANVAS_WIDTH + ((i / 4) % CANVAS_WIDTH);
-// 	const y = Math.floor(i / 4 / CANVAS_WIDTH);
-// 	const b = ctx.getImageData(x, y, 1, 1).data;
-
-// 	if (a[i] !== b[0] || a[i + 2] !== b[2]) {
-// 		return false;
-// 	}
-
-// 	// Some emojis are a contraction of different ones, so if it's not
-// 	// supported, it will show multiple characters
-// 	if (ctx.measureText(emoji).width >= CANVAS_WIDTH) {
-// 		return false;
-// 	}
-
-// 	// Supported
-// 	return true;
-// };
-
-// const supportedEmojis = new Map<string, boolean>();
-
-// export const isEmojiSupported = (emoji: string): boolean => {
-// 	let cached = supportedEmojis.get(emoji);
-
-// 	if (cached === undefined) {
-// 		supportedEmojis.set(emoji, (cached = isEmojiSupportedUncached(emoji)));
-// 	}
-
-// 	return cached;
-// };
-
-// export const hasZwj = (emoji: string): boolean => {
-// 	return emoji.includes('\u200d');
-// };
-
-export const findEmojiSupportLevel = () => {
-	const isEmojiSupported = (emoji: string): boolean => {
-		ctx.clearRect(0, 0, CANVAS_WIDTH * 2, CANVAS_HEIGHT);
-
-		// Draw in red on the left
-		ctx.fillStyle = '#FF0000';
-		ctx.fillText(emoji, 0, 22);
-
-		// Draw in blue on right
-		ctx.fillStyle = '#0000FF';
-		ctx.fillText(emoji, CANVAS_WIDTH, 22);
-
-		const a = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT).data;
-		const count = a.length;
-		let i = 0;
-
-		// Search the first visible pixel
-		for (; i < count && !a[i + 3]; i += 4);
-
-		// No visible pixel
-		if (i >= count) {
-			return false;
-		}
-
-		// Emoji has immutable color, so we check the color of the emoji in two different colors
-		// the result show be the same.
-		const x = CANVAS_WIDTH + ((i / 4) % CANVAS_WIDTH);
-		const y = Math.floor(i / 4 / CANVAS_WIDTH);
-		const b = ctx.getImageData(x, y, 1, 1).data;
-
-		if (a[i] !== b[0] || a[i + 2] !== b[2]) {
-			return false;
-		}
-
-		// Some emojis are a contraction of different ones, so if it's not
-		// supported, it will show multiple characters
-		if (ctx.measureText(emoji).width >= CANVAS_WIDTH) {
-			return false;
-		}
-
-		// Supported
-		return true;
-	};
-
-	const ctx = document.createElement('canvas').getContext('2d')!;
-
-	// Set the canvas up for reuse
-	ctx.font = `${FONT_SIZE}px ${FONT_FAMILY}`;
+	const ctx = canvas.getContext('2d')!;
 	ctx.textBaseline = 'top';
-	ctx.canvas.width = CANVAS_WIDTH * 2;
-	ctx.canvas.height = CANVAS_HEIGHT;
+	ctx.font = `100px ${FONT_FAMILY}`;
+	ctx.fillStyle = color;
+	ctx.scale(0.01, 0.01);
+	ctx.fillText(text, 0, 0);
 
-	for (const [emoji, version] of emojiVersions) {
-		if (isEmojiSupported(emoji)) {
-			return version;
-		}
-	}
+	return ctx.getImageData(0, 0, 1, 1).data;
+};
 
-	throw new Error(`Emojis not supported?`);
+const compareFeatures = (feature1: Uint8ClampedArray, feature2: Uint8ClampedArray) => {
+	const feature1Str = [...feature1].join(',');
+	const feature2Str = [...feature2].join(',');
+
+	// This is RGBA, so for 0,0,0, we are checking that the first RGB is not all zeroes.
+	// Most of the time when unsupported this is 0,0,0,0, but on Chrome on Mac it is
+	// 0,0,0,61 - there is a transparency here.
+	return feature1Str === feature2Str && !feature1Str.startsWith('0,0,0,');
+};
+
+const isEmojiSupported = (text: string) => {
+	const feature1 = getTextFeature(text, '#000');
+	const feature2 = getTextFeature(text, '#fff');
+	return feature1 && feature2 && compareFeatures(feature1, feature2);
 };
 
 let promise: Promise<number>;
@@ -191,7 +71,11 @@ export const detectEmojiSupportLevel = (): Promise<number> => {
 	return (promise ||= new Promise((resolve) => {
 		scheduleIdleTask(() => {
 			try {
-				resolve(findEmojiSupportLevel());
+				for (const [emoji, version] of emojiVersions) {
+					if (isEmojiSupported(emoji)) {
+						return resolve(version);
+					}
+				}
 			} catch {
 				// ignore canvas errors
 			}
