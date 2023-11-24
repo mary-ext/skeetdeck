c66d06db2f4d52e57b2701c312bb356a7bb0d567
Mary <pineapplecreamcheese@skiff.com>
2023-11-24 10:38:21 +0700

    fix: change timeline limit to 30

--- app/api/queries/get-timeline.ts
+++ app/api/queries/get-timeline.ts
@@ -89,7 +89,7 @@ type PostRecord = Records['app.bsky.feed.post'];
 // How many attempts it should try looking for more items before it gives up on empty pages.
 const MAX_EMPTY = 3;
 
-const MAX_POSTS = 20;
+const MAX_POSTS = 30;
 
 const countPosts = (slices: TimelineSlice[], limit?: number) => {
 	let count = 0;
