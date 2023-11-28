61bd92a96d77fde344e896e21d12b33a50ee76f2
Mary <pineapplecreamcheese@skiff.com>
2023-11-28 14:05:06 +0700

    refactor: redo list header

--- app/desktop/components/panes/views/CustomListPane.tsx
+++ app/desktop/components/panes/views/CustomListPane.tsx
@@ -5,6 +5,8 @@ import { createQuery } from '@pkg/solid-query';
 import type { DID } from '~/api/atp-schema.ts';
 import { getInitialListInfo, getListInfo, getListInfoKey } from '~/api/queries/get-list-info.ts';
 
+import { openModal } from '~/com/globals/modals.tsx';
+
 import type { CustomListPaneConfig } from '../../../globals/panes.ts';
 
 import { Button } from '~/com/primitives/button.ts';
@@ -25,8 +27,10 @@ import PaneAside from '../PaneAside.tsx';
 import PaneBody from '../PaneBody.tsx';
 import PaneHeader from '../PaneHeader.tsx';
 
-import ListSettingsPaneDialog from '../dialogs/ListSettingsPaneDialog.tsx';
 import GenericPaneSettings from '../settings/GenericPaneSettings.tsx';
+
+import ListMembersPaneDialog from '../dialogs/ListMembersPaneDialog.tsx';
+import ListSettingsPaneDialog from '../dialogs/ListSettingsPaneDialog.tsx';
 
 const LazyImageViewerDialog = lazy(() => import('~/com/components/dialogs/ImageViewerDialog.tsx'));
 
@@ -81,7 +85,7 @@ const CustomListPane = () => {
 export default CustomListPane;
 
 const ListHeader = (props: { uid: DID; uri: string }) => {
-	const { openModal } = usePaneContext();
+	const { openModal: openPaneModal } = usePaneContext();
 
 	const list = createQuery(() => {
 		const key = getListInfoKey(props.uid, props.uri);
@@ -102,60 +106,72 @@ const ListHeader = (props: { uid: DID; uri: string }) => {
 
 			return (
 				<VirtualContainer class="shrink-0">
-					<div class="border-b border-divider">
-						{(() => {
-							const avatar = data.avatar.value;
+					<div class="flex flex-col gap-4 border-b border-divider p-4">
+						<div class="flex gap-4">
+							{(() => {
+								const avatar = data.avatar.value;
 
-							if (avatar) {
-								return (
-									<button
-										onClick={() => {
-											openModal(() => <LazyImageViewerDialog images={[{ fullsize: avatar }]} />);
-										}}
-										class="group block aspect-banner bg-background"
-									>
-										<img src={avatar} class="h-full w-full object-cover group-hover:opacity-75" />
-									</button>
-								);
-							}
+								if (avatar) {
+									return (
+										<button
+											onClick={() => {
+												openModal(() => <LazyImageViewerDialog images={[{ fullsize: avatar }]} />);
+											}}
+											class="group h-13 w-13 shrink-0 overflow-hidden rounded-md bg-background"
+										>
+											<img src={avatar} class="h-full w-full object-cover group-hover:opacity-75" />
+										</button>
+									);
+								}
 
-							return <div class="aspect-banner bg-muted-fg"></div>;
-						})()}
+								return <div class="h-13 w-13 shrink-0 rounded-md bg-muted-fg"></div>;
+							})()}
 
-						<div class="flex flex-col items-center gap-3 p-3">
-							<p class="text-center text-lg font-bold">{data.name.value}</p>
-							<p class="whitespace-pre-wrap break-words text-center text-sm empty:hidden">
-								{data.description.value}
-							</p>
+							<div class="grow">
+								<p class="break-words text-lg font-bold">{data.name.value}</p>
 
-							<Link
-								to={/* @once */ { type: LinkingType.PROFILE, actor: creator.did }}
-								class="group flex items-center text-left"
-							>
-								<img src={creator.avatar.value || DefaultAvatar} class="mr-2 h-5 w-5 rounded-full" />
-								<span class="mr-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold empty:hidden group-hover:underline">
-									{creator.displayName.value}
-								</span>
-								<span class="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-muted-fg">
-									@{creator.handle.value}
-								</span>
-							</Link>
+								<Link
+									to={/* @once */ { type: LinkingType.PROFILE, actor: creator.did }}
+									class="group mt-1 flex items-center text-left"
+								>
+									<img src={creator.avatar.value || DefaultAvatar} class="mr-2 h-5 w-5 rounded-full" />
+									<span class="mr-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold empty:hidden group-hover:underline">
+										{creator.displayName.value}
+									</span>
+									<span class="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-muted-fg">
+										@{creator.handle.value}
+									</span>
+								</Link>
+							</div>
+						</div>
 
+						<p class="whitespace-pre-wrap break-words text-sm empty:hidden">{data.description.value}</p>
+
+						<div class="flex gap-2">
 							{(() => {
 								if (props.uid === data.creator.uid) {
 									return (
-										<div class="my-1 flex gap-3">
-											<button
-												onClick={() => {
-													openModal(() => <ListSettingsPaneDialog list={data} />);
-												}}
-												class={/* @once */ Button({ variant: 'outline' })}
-											>
-												Edit list
-											</button>
-										</div>
+										<button
+											onClick={() => {
+												openPaneModal(() => <ListSettingsPaneDialog list={data} />);
+											}}
+											class={/* @once */ Button({ variant: 'outline' })}
+										>
+											Edit list
+										</button>
 									);
 								}
+
+								return (
+									<button
+										onClick={() => {
+											openPaneModal(() => <ListMembersPaneDialog list={data} />);
+										}}
+										class={/* @once */ Button({ variant: 'outline' })}
+									>
+										View members
+									</button>
+								);
 							})()}
 						</div>
 					</div>
@@ -164,9 +180,8 @@ const ListHeader = (props: { uid: DID; uri: string }) => {
 		}
 
 		return (
-			<div class="shrink-0 border-b border-divider">
-				<div class="aspect-banner bg-secondary/20"></div>
-				<div style="height:172px"></div>
+			<div class="shrink-0 border-b border-divider" style="height:172.8px">
+				<div class="m-4 h-13 w-13 shrink-0 rounded-md bg-secondary/20"></div>
 			</div>
 		);
 	}) as unknown as JSX.Element;
