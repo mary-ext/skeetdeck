fdbc2acb4257a5e3bbb52e2dd6049d3b9b005bc0
Mary <pineapplecreamcheese@skiff.com>
2023-11-30 12:04:40 +0700

    feat: add account actions

--- app/desktop/components/settings/SettingsDialog.tsx
+++ app/desktop/components/settings/SettingsDialog.tsx
@@ -1,13 +1,19 @@
-import { For, Show } from 'solid-js';
+import { For, JSX, Show } from 'solid-js';
 
+import { useQueryClient } from '@pkg/solid-query';
+
+import type { MultiagentAccountData } from '~/api/classes/multiagent.ts';
 import { multiagent } from '~/api/globals/agent.js';
 
 import { closeModal, openModal } from '~/com/globals/modals.tsx';
 
-import DialogOverlay from '~/com/components/dialogs/DialogOverlay.tsx';
-
 import { DialogBody, DialogHeader, DialogRoot, DialogTitle } from '~/com/primitives/dialog.ts';
 import { IconButton } from '~/com/primitives/icon-button.ts';
+import { MenuItem, MenuRoot } from '~/com/primitives/menu.ts';
+
+import { Flyout } from '~/com/components/Flyout.tsx';
+import ConfirmDialog from '~/com/components/dialogs/ConfirmDialog.tsx';
+import DialogOverlay from '~/com/components/dialogs/DialogOverlay.tsx';
 
 import CloseIcon from '~/com/icons/baseline-close.tsx';
 import MoreHorizIcon from '~/com/icons/baseline-more-horiz.tsx';
@@ -72,9 +78,11 @@ const SettingsDialog = () => {
 										</Show>
 
 										<div>
-											<button class={/* @once */ IconButton({ edge: 'right', color: 'muted' })}>
-												<MoreHorizIcon />
-											</button>
+											<AccountActionMenu account={account}>
+												<button class={/* @once */ IconButton({ edge: 'right', color: 'muted' })}>
+													<MoreHorizIcon />
+												</button>
+											</AccountActionMenu>
 										</div>
 									</div>
 								);
@@ -88,3 +96,63 @@ const SettingsDialog = () => {
 };
 
 export default SettingsDialog;
+
+interface AccountActionMenuProps {
+	/** Expected to be static */
+	account: MultiagentAccountData;
+	children: JSX.Element;
+}
+
+const AccountActionMenu = (props: AccountActionMenuProps) => {
+	const queryClient = useQueryClient();
+
+	const account = props.account;
+	const did = account.did;
+
+	return (
+		<Flyout button={props.children}>
+			{({ close, menuProps }) => (
+				<div {...menuProps} class={/* @once */ MenuRoot()}>
+					<button
+						disabled={did === multiagent.active}
+						onClick={() => {
+							close();
+							multiagent.active = did;
+						}}
+						class={/* @once */ MenuItem()}
+					>
+						Set as default
+					</button>
+					<button
+						onClick={() => {
+							close();
+
+							openModal(() => (
+								<ConfirmDialog
+									title={`Sign out?`}
+									body={`This will sign you out of @${
+										account.profile?.handle || account.session.handle
+									}, and you'll still be signed in to other accounts.`}
+									confirmation={`Sign out`}
+									onConfirm={() => {
+										multiagent.logout(did);
+
+										queryClient.resetQueries({
+											predicate: (query) => {
+												const key = query.queryKey;
+												return key.length >= 2 && key[1] === did && !(key[0] as string).includes('/');
+											},
+										});
+									}}
+								/>
+							));
+						}}
+						class={/* @once */ MenuItem()}
+					>
+						Sign out
+					</button>
+				</div>
+			)}
+		</Flyout>
+	);
+};
