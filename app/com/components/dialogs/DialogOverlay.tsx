9c35d39269ed72f9c93991e3686124d3c2d18635
Mary <pineapplecreamcheese@skiff.com>
2023-12-01 14:17:10 +0700

    fix: increase dialog boundary padding

--- app/com/components/dialogs/DialogOverlay.tsx
+++ app/com/components/dialogs/DialogOverlay.tsx
@@ -11,7 +11,7 @@ const DialogOverlay = (props: DialogOverlayProps) => {
 		<div
 			class={
 				`overlay-y-auto pointer-events-none flex h-full w-full justify-center overflow-y-auto bg-black/50 dark:bg-hinted/50` +
-				(isDesktop ? ` items-center p-6` : ` items-end sm:items-center sm:p-6`)
+				(isDesktop ? ` items-center p-8` : ` items-end sm:items-center sm:p-8`)
 			}
 		>
 			<div class="pointer-events-auto contents">{props.children}</div>
