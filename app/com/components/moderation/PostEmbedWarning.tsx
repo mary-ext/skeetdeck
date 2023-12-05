4caee567ab564a65385b664f705411238b4a7155
Mary <pineapplecreamcheese@skiff.com>
2023-12-05 11:09:52 +0700

    refactor: touch up media warning ui

--- app/com/components/moderation/PostEmbedWarning.tsx
+++ app/com/components/moderation/PostEmbedWarning.tsx
@@ -1,9 +1,12 @@
 import { type JSX, createMemo, createSignal } from 'solid-js';
 
+import { renderLabelNames } from '~/api/display.ts';
 import type { SignalizedPost } from '~/api/stores/posts.ts';
 
 import { CauseLabel } from '~/api/moderation/action.ts';
 import { getPostModMaker } from '~/api/moderation/decisions/post.ts';
+
+import ShieldIcon from '../../icons/baseline-shield.tsx';
 
 import { useSharedPreferences } from '../SharedPreferences.tsx';
 
@@ -36,15 +39,16 @@ const PostEmbedWarning = (props: PostEmbedWarningProps) => {
 		const [show, setShow] = createSignal(false);
 
 		const source = $decision.s;
-		const title = `Media warning${source.t === CauseLabel ? `: ${source.l.val}` : ''}`;
+		const title = source.t === CauseLabel ? renderLabelNames(source.l.val) : `Media warning`;
 
 		return [
-			<div class="mt-3 flex items-stretch justify-between gap-3 overflow-hidden rounded-md border border-divider">
-				<p class="m-3 text-sm text-muted-fg">{title}</p>
+			<div class="mt-3 flex min-w-0 items-center gap-3 overflow-hidden rounded-md border border-divider">
+				<ShieldIcon class="ml-3 text-base" />
+				<span class="grow text-sm">{title}</span>
 
 				<button
 					onClick={() => setShow(!show())}
-					class="px-4 text-sm font-medium hover:bg-secondary/30 hover:text-secondary-fg"
+					class="p-3 text-de font-medium text-accent hover:bg-secondary/30"
 				>
 					{show() ? 'Hide' : 'Show'}
 				</button>
