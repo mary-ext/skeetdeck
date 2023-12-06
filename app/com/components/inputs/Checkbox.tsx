27a1dd8b636076cf1c544c65bfeabc7e17ef5114
Mary <pineapplecreamcheese@skiff.com>
2023-12-06 11:48:51 +0700

    fix: add noticeable outline for checkbox and radio

--- app/com/components/inputs/Checkbox.tsx
+++ app/com/components/inputs/Checkbox.tsx
@@ -7,7 +7,7 @@ const Checkbox = (props: ComponentProps<'input'>) => {
 		<label class="relative inline-flex shrink-0 cursor-pointer text-xl">
 			<input {...props} type="checkbox" class="peer h-0 w-0 appearance-none leading-none outline-none" />
 
-			<div class="pointer-events-none absolute -inset-2 rounded-full peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20" />
+			<div class="pointer-events-none absolute -inset-2 rounded-full outline-2 -outline-offset-2 outline-accent peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20 peer-focus-visible:outline" />
 
 			<div class="z-10 h-5 w-5 rounded border-2 border-secondary bg-background peer-checked:hidden"></div>
 			<div class="z-10 hidden h-5 w-5 place-items-center rounded bg-accent peer-checked:grid">
--- app/com/components/inputs/Radio.tsx
+++ app/com/components/inputs/Radio.tsx
@@ -5,7 +5,7 @@ const Radio = (props: ComponentProps<'input'>) => {
 		<label class="relative inline-flex h-5 w-5 shrink-0 cursor-pointer text-xl">
 			<input {...props} type="radio" class="peer h-0 w-0 appearance-none leading-none outline-none" />
 
-			<div class="pointer-events-none absolute -inset-2 rounded-full peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20" />
+			<div class="pointer-events-none absolute -inset-2 rounded-full outline-2 -outline-offset-2 outline-accent peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20 peer-focus-visible:outline" />
 
 			<div class="z-10 h-5 w-5 rounded-full border-2 border-secondary bg-background peer-checked:hidden"></div>
 			<div class="z-10 hidden h-5 w-5 place-items-center rounded-full bg-accent peer-checked:grid">
