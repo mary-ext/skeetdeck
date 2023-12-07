9aad308a541d2ef570f05f9a82ecde92914e83de
Mary <pineapplecreamcheese@skiff.com>
2023-12-07 09:32:25 +0700

    fix: properly disable checkbox and radio input

--- app/com/components/inputs/Checkbox.tsx
+++ app/com/components/inputs/Checkbox.tsx
@@ -7,10 +7,10 @@ const Checkbox = (props: ComponentProps<'input'>) => {
 		<label class="relative inline-flex shrink-0 cursor-pointer text-xl">
 			<input {...props} type="checkbox" class="peer h-0 w-0 appearance-none leading-none outline-none" />
 
-			<div class="pointer-events-none absolute -inset-2 rounded-full outline-2 -outline-offset-2 outline-accent peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20 peer-focus-visible:outline" />
+			<div class="pointer-events-none absolute -inset-2 rounded-full outline-2 -outline-offset-2 outline-accent peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20 peer-focus-visible:outline peer-disabled:hidden" />
 
-			<div class="z-10 h-5 w-5 rounded border-2 border-secondary bg-background peer-checked:hidden"></div>
-			<div class="z-10 hidden h-5 w-5 place-items-center rounded bg-accent peer-checked:grid">
+			<div class="z-10 h-5 w-5 rounded border-2 border-secondary bg-background peer-checked:hidden peer-disabled:opacity-50"></div>
+			<div class="z-10 hidden h-5 w-5 place-items-center rounded bg-accent peer-checked:grid peer-disabled:opacity-50">
 				<CheckIcon class="stroke-white stroke-1 text-sm text-white" />
 			</div>
 		</label>
--- app/com/components/inputs/Radio.tsx
+++ app/com/components/inputs/Radio.tsx
@@ -5,10 +5,10 @@ const Radio = (props: ComponentProps<'input'>) => {
 		<label class="relative inline-flex h-5 w-5 shrink-0 cursor-pointer text-xl">
 			<input {...props} type="radio" class="peer h-0 w-0 appearance-none leading-none outline-none" />
 
-			<div class="pointer-events-none absolute -inset-2 rounded-full outline-2 -outline-offset-2 outline-accent peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20 peer-focus-visible:outline" />
+			<div class="pointer-events-none absolute -inset-2 rounded-full outline-2 -outline-offset-2 outline-accent peer-hover:bg-secondary/20 peer-focus-visible:bg-secondary/20 peer-focus-visible:outline peer-disabled:hidden" />
 
-			<div class="z-10 h-5 w-5 rounded-full border-2 border-secondary bg-background peer-checked:hidden"></div>
-			<div class="z-10 hidden h-5 w-5 place-items-center rounded-full bg-accent peer-checked:grid">
+			<div class="z-10 h-5 w-5 rounded-full border-2 border-secondary bg-background peer-checked:hidden peer-disabled:opacity-50"></div>
+			<div class="z-10 hidden h-5 w-5 place-items-center rounded-full bg-accent peer-checked:grid peer-disabled:opacity-50">
 				<div class="h-2 w-2 rounded-full bg-white"></div>
 			</div>
 		</label>
