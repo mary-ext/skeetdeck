4e06a0a62dcf297ee81e89eb4c842cf00a4637a3
Mary <pineapplecreamcheese@skiff.com>
2023-12-08 14:17:16 +0700

    feat: ShowFreeze component

--- packages/solid-freeze/lib/index.tsx
+++ packages/solid-freeze/lib/index.tsx
@@ -59,3 +59,45 @@ export const Freeze = (props: FreezeProps) => {
 		/>
 	);
 };
+
+export interface ShowFreezeProps {
+	when: boolean;
+	children: JSX.Element;
+	fallback?: JSX.Element;
+}
+
+export const ShowFreeze = (props: ShowFreezeProps) => {
+	const promise = createMemo((prev: Deferred | undefined) => {
+		if (props.when) {
+			if (prev) {
+				return prev;
+			}
+
+			let _resolve: Deferred['r'];
+			let promise = new Promise((resolve) => (_resolve = resolve)) as Deferred;
+
+			promise.r = _resolve!;
+			return promise;
+		} else if (prev) {
+			prev.r(undefined);
+		}
+	});
+
+	const show = createMemo((prev: boolean) => {
+		if (prev || props.when) {
+			return prev;
+		}
+
+		return prev;
+	}, false);
+
+	const [suspend] = createResource(promise, identity);
+
+	return (
+		<Suspense
+			fallback={props.fallback}
+			// @ts-expect-error
+			children={[suspend, () => show() && props.children]}
+		/>
+	);
+};
