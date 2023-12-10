6b1151552bfa6d2d233547232eb099e00f111488
Mary <pineapplecreamcheese@skiff.com>
2023-12-10 16:00:44 +0700

    fix: alt indicator on light theme

--- app/com/components/embeds/EmbedImage.tsx
+++ app/com/components/embeds/EmbedImage.tsx
@@ -98,7 +98,7 @@ const EmbedImage = (props: EmbedImageProps) => {
 				{interactive && alt && (
 					<ImageAltAction alt={alt}>
 						<button
-							class="absolute bottom-0 left-0 m-2 h-5 rounded bg-black/70 px-1 text-xs font-medium"
+							class="absolute bottom-0 left-0 m-2 h-5 rounded bg-black/70 px-1 text-xs font-medium text-white"
 							title="Show image description"
 						>
 							ALT
