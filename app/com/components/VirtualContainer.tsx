0989d521237a5b714d62372f2a592a0c358dfa68
Mary <pineapplecreamcheese@skiff.com>
2023-12-17 16:11:44 +0700

    refactor: use ResizeObserver for VirtualContainer

--- app/com/components/VirtualContainer.tsx
+++ app/com/components/VirtualContainer.tsx
@@ -1,15 +1,13 @@
 import { type JSX, createSignal, onCleanup } from 'solid-js';
 
 import { scheduleIdleTask } from '~/utils/idle.ts';
-import { getRectFromEntry, scrollObserver } from '~/utils/intersection-observer.ts';
+import { resizeObserver, scrollObserver } from '~/utils/intersection-observer.ts';
 
 export interface VirtualContainerProps {
 	estimateHeight?: number;
 	class?: string;
 	children?: JSX.Element;
 }
-
-const isMobile = import.meta.env.VITE_MODE !== 'desktop' && /Android/.test(navigator.userAgent);
 
 export const VirtualContainer = (props: VirtualContainerProps) => {
 	let entry: IntersectionObserverEntry | undefined;
@@ -20,13 +18,7 @@ export const VirtualContainer = (props: VirtualContainerProps) => {
 	const [intersecting, setIntersecting] = createSignal(false);
 	const [cachedHeight, setCachedHeight] = createSignal(estimateHeight);
 
-	const calculateHeight = () => {
-		const nextHeight = getRectFromEntry(entry!).height;
-
-		if (nextHeight !== height) {
-			setCachedHeight((height = nextHeight));
-		}
-	};
+	const shouldHide = () => !intersecting() && cachedHeight() !== undefined;
 
 	const handleIntersect = (nextEntry: IntersectionObserverEntry) => {
 		const prev = intersecting();
@@ -34,77 +26,46 @@ export const VirtualContainer = (props: VirtualContainerProps) => {
 
 		entry = nextEntry;
 
-		// @todo: figure out why this is at all.
-		// after scratching my head on and off for 12 hours, I've concluded that:
-		//
-		// 1. the broken assumption around how the virtual container attempts to
-		//    store the height of items somehow makes scroll restoration work.
-		//
-		// 2. fixing this broken assumption somehow breaks scroll restoration.
-		//
-		// 3. this only happens on mobile browsers.
-		//
-		// my only assumption is that mobile phones are slow, but I am not willing
-		// to spend more hours on this for now.
-
-		if (!isMobile) {
-			// new behavior.
-
-			if (!prev && next) {
-				// hidden -> visible
-				// immediately mount the children, then schedule a task to update the
-				// cached height, this handles the scenario where the virtual container
-				// gets unmounted without ever being out-of-bounds.
-				setIntersecting(next);
-
-				// @todo: this is more meant for mobile web app where navigation blows
-				// the entire page away, if we're making use of Suspense for offscreen
-				// rendering then this shouldn't be necessary at all.
-				if (isMobile) {
-					scheduleIdleTask(() => {
-						// bail out if it's no longer us.
-						if (entry !== nextEntry) {
-							return;
-						}
-
-						calculateHeight();
-					});
-				}
-			} else if (prev && !next) {
-				// visible -> hidden
-				// unmounting is cheap, but we don't need to immediately unmount it, say
-				// for scenarios where layout is still being figured out and we don't
-				// actually know where the virtual container is gonna end up.
-				scheduleIdleTask(() => {
-					// bail out if it's no longer us.
-					if (entry !== nextEntry) {
-						return;
-					}
-
-					calculateHeight();
-					setIntersecting(next);
-				});
-			}
-		} else {
-			// old behavior.
-
-			if (!prev && next) {
-				// hidden -> visible
-				scheduleIdleTask(calculateHeight);
-			}
-
+		if (!prev && next) {
+			// hidden -> visible
 			setIntersecting(next);
+		} else if (prev && !next) {
+			// visible -> hidden
+			// unmounting is cheap, but we don't need to immediately unmount it, say
+			// for scenarios where layout is still being figured out and we don't
+			// actually know where the virtual container is gonna end up.
+			scheduleIdleTask(() => {
+				// bail out if it's no longer us.
+				if (entry !== nextEntry) {
+					return;
+				}
+
+				setIntersecting(next);
+			});
 		}
 	};
 
-	const shouldHide = () => !intersecting() && cachedHeight() !== undefined;
+	const handleResize = (nextEntry: ResizeObserverEntry) => {
+		if (shouldHide()) {
+			return;
+		}
+
+		const contentRect = nextEntry.contentRect;
+		const nextHeight = Math.trunc(contentRect.height * 1000) / 1000;
+
+		if (nextHeight !== height) {
+			setCachedHeight((height = nextHeight));
+		}
+	};
 
 	return (
 		<article
 			ref={startMeasure}
 			class={props.class}
 			style={{ height: shouldHide() ? `${height ?? cachedHeight()}px` : undefined }}
+			data-cached={cachedHeight()}
 			prop:$onintersect={handleIntersect}
+			prop:$onresize={handleResize}
 		>
 			{(() => {
 				if (!shouldHide()) {
@@ -117,8 +78,10 @@ export const VirtualContainer = (props: VirtualContainerProps) => {
 
 const startMeasure = (node: HTMLElement) => {
 	scrollObserver.observe(node);
+	resizeObserver.observe(node);
 
 	onCleanup(() => {
 		scrollObserver.unobserve(node);
+		resizeObserver.unobserve(node);
 	});
 };
--- app/utils/intersection-observer.ts
+++ app/utils/intersection-observer.ts
@@ -1,23 +1,4 @@
-let hasBoundingRectBug: boolean | undefined;
-
-export const getRectFromEntry = (entry: IntersectionObserverEntry) => {
-	if (hasBoundingRectBug === undefined) {
-		const boundingRect = entry.target.getBoundingClientRect();
-		const observerRect = entry.boundingClientRect;
-
-		hasBoundingRectBug =
-			boundingRect.height !== observerRect.height ||
-			boundingRect.top !== observerRect.top ||
-			boundingRect.width !== observerRect.width ||
-			boundingRect.bottom !== observerRect.bottom ||
-			boundingRect.left !== observerRect.left ||
-			boundingRect.right !== observerRect.right;
-	}
-
-	return hasBoundingRectBug ? entry.target.getBoundingClientRect() : entry.boundingClientRect;
-};
-
-const callback: IntersectionObserverCallback = (entries, observer) => {
+const intersectionCallback: IntersectionObserverCallback = (entries, observer) => {
 	for (let idx = 0, len = entries.length; idx < len; idx++) {
 		const entry = entries[idx];
 
@@ -32,12 +13,30 @@ const callback: IntersectionObserverCallback = (entries, observer) => {
 	}
 };
 
-export const scrollObserver = new IntersectionObserver(callback, { rootMargin: '106.25% 0px' });
+const resizeCallback: ResizeObserverCallback = (entries, observer) => {
+	for (let idx = 0, len = entries.length; idx < len; idx++) {
+		const entry = entries[idx];
+
+		const target = entry.target as any;
+		const listener = target.$onresize;
+
+		if (listener) {
+			listener(entry);
+		} else {
+			observer.unobserve(target);
+		}
+	}
+};
+
+export const scrollObserver = new IntersectionObserver(intersectionCallback, { rootMargin: '106.25% 0px' });
+
+export const resizeObserver = new ResizeObserver(resizeCallback);
 
 declare module 'solid-js' {
 	namespace JSX {
 		interface ExplicitProperties {
 			$onintersect: (entry: IntersectionObserverEntry) => void;
+			$onresize: (entry: ResizeObserverEntry) => void;
 		}
 	}
 }
