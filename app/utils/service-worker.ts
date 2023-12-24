31f80b4c1e2e8f199361b5ebfff61baa73158e32
Mary <pineapplecreamcheese@skiff.com>
2023-12-24 07:49:16 +0700

    chore: easy way to disable sw

--- app/utils/service-worker.ts
+++ app/utils/service-worker.ts
@@ -1,12 +1,16 @@
 import { createSignal } from 'solid-js';
 import { registerSW } from 'virtual:pwa-register';
 
+const REGISTER_SW = true;
+
 const [isUpdateReady, setIsUpdateReady] = createSignal(false);
 
-const updateSW = registerSW({
-	onNeedRefresh() {
-		setIsUpdateReady(true);
-	},
-});
+const updateSW = REGISTER_SW
+	? registerSW({
+			onNeedRefresh() {
+				setIsUpdateReady(true);
+			},
+		})
+	: () => {};
 
 export { isUpdateReady, updateSW };
