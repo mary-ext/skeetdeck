dbdcfe2ccbc032ada090f16947294b1d33a14b7e
Mary <pineapplecreamcheese@skiff.com>
2023-12-30 13:59:10 +0700

    chore: expose post cache on canary

--- app/api/stores/posts.ts
+++ app/api/stores/posts.ts
@@ -14,10 +14,18 @@ const gc = new FinalizationRegistry<string>((id) => {
 	const ref = posts[id];
 
 	if (!ref || !ref.deref()) {
-		console.log(`removing post ${id}`);
+		if (import.meta.env.VITE_GIT_BRANCH === 'canary') {
+			console.log(`removing post ${id}`);
+		}
+
 		delete posts[id];
 	}
 });
+
+if (import.meta.env.VITE_GIT_BRANCH === 'canary') {
+	// @ts-expect-error
+	window._posts = posts;
+}
 
 export class SignalizedPost {
 	readonly uid: DID;
