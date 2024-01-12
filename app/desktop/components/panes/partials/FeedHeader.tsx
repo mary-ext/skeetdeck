f10e695616702f82ddff87571efe450ece880ef6
Mary <pineapplecreamcheese@skiff.com>
2024-01-12 17:19:17 +0700

    fix: add virtualcontainer to feedheader

--- app/desktop/components/panes/partials/FeedHeader.tsx
+++ app/desktop/components/panes/partials/FeedHeader.tsx
@@ -13,6 +13,7 @@ import { Button } from '~/com/primitives/button.ts';
 
 import { LINK_FEED_LIKED_BY, LINK_PROFILE, Link } from '~/com/components/Link.tsx';
 import RichTextRenderer from '~/com/components/RichTextRenderer.tsx';
+import { VirtualContainer } from '~/com/components/VirtualContainer.tsx';
 
 import FavoriteIcon from '~/com/icons/baseline-favorite.tsx';
 import FavoriteOutlinedIcon from '~/com/icons/outline-favorite.tsx';
@@ -45,85 +46,87 @@ const FeedHeader = (props: FeedHeaderProps) => {
 		const isLiked = () => !!feed.viewer.like.value;
 
 		return (
-			<div class="flex flex-col gap-3 p-4">
-				<div class="flex gap-4">
-					{(() => {
-						const avatar = feed.avatar.value;
+			<VirtualContainer class="shrink-0">
+				<div class="flex flex-col gap-3 p-4">
+					<div class="flex gap-4">
+						{(() => {
+							const avatar = feed.avatar.value;
 
-						if (avatar) {
-							return (
-								<button
-									onClick={() => {
-										openModal(() => <LazyImageViewerDialog images={[{ fullsize: avatar }]} />);
-									}}
-									class="group h-13 w-13 shrink-0 overflow-hidden rounded-md bg-background"
-								>
-									<img src={avatar} class="h-full w-full object-cover group-hover:opacity-75" />
-								</button>
-							);
-						}
+							if (avatar) {
+								return (
+									<button
+										onClick={() => {
+											openModal(() => <LazyImageViewerDialog images={[{ fullsize: avatar }]} />);
+										}}
+										class="group h-13 w-13 shrink-0 overflow-hidden rounded-md bg-background"
+									>
+										<img src={avatar} class="h-full w-full object-cover group-hover:opacity-75" />
+									</button>
+								);
+							}
 
-						return <img src={DefaultFeedAvatar} class="h-13 w-13 shrink-0 rounded-md" />;
-					})()}
+							return <img src={DefaultFeedAvatar} class="h-13 w-13 shrink-0 rounded-md" />;
+						})()}
 
-					<div class="flex min-w-0 grow flex-col">
-						<p class="break-words text-lg font-bold">{feed.name.value}</p>
+						<div class="flex min-w-0 grow flex-col">
+							<p class="break-words text-lg font-bold">{feed.name.value}</p>
 
-						<Link
-							to={/* @once */ { type: LINK_PROFILE, actor: creator.did }}
-							class="group mt-1 flex items-center text-left"
+							<Link
+								to={/* @once */ { type: LINK_PROFILE, actor: creator.did }}
+								class="group mt-1 flex items-center text-left"
+							>
+								<img src={creator.avatar.value || DefaultUserAvatar} class="mr-2 h-5 w-5 rounded-full" />
+								<span class="mr-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold empty:hidden group-hover:underline">
+									{creator.displayName.value}
+								</span>
+								<span class="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-muted-fg">
+									@{creator.handle.value}
+								</span>
+							</Link>
+						</div>
+					</div>
+
+					<p class="whitespace-pre-wrap break-words text-sm empty:hidden">
+						<RichTextRenderer
+							item={feed}
+							get={(item) => {
+								return { t: item.description.value || '', f: item.descriptionFacets.value };
+							}}
+						/>
+					</p>
+
+					<Link
+						to={{ type: LINK_FEED_LIKED_BY, actor: creator.did, rkey: getRecordId(feed.uri) }}
+						class="text-left text-sm text-muted-fg hover:underline"
+					>
+						Liked by {formatCompact(feed.likeCount.value)} users
+					</Link>
+
+					<div class="flex gap-2">
+						<button
+							title={!isLiked() ? `Like this feed` : `Unlike this feed`}
+							onClick={() => {
+								updateFeedLike(feed, !isLiked());
+							}}
+							class={/* @once */ Button({ variant: 'outline' })}
 						>
-							<img src={creator.avatar.value || DefaultUserAvatar} class="mr-2 h-5 w-5 rounded-full" />
-							<span class="mr-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold empty:hidden group-hover:underline">
-								{creator.displayName.value}
-							</span>
-							<span class="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-muted-fg">
-								@{creator.handle.value}
-							</span>
-						</Link>
+							{isLiked() ? (
+								<FavoriteIcon class="-mx-1.5 text-base text-red-500" />
+							) : (
+								<FavoriteOutlinedIcon class="-mx-1.5 text-base" />
+							)}
+						</button>
+
+						<div class="grow"></div>
+
+						<FeedOverflowAction feed={feed}>
+							<button title="Actions" class={/* @once */ Button({ variant: 'outline' })}>
+								<MoreHorizIcon class="-mx-1.5 text-base" />
+							</button>
+						</FeedOverflowAction>
 					</div>
 				</div>
-
-				<p class="whitespace-pre-wrap break-words text-sm empty:hidden">
-					<RichTextRenderer
-						item={feed}
-						get={(item) => {
-							return { t: item.description.value || '', f: item.descriptionFacets.value };
-						}}
-					/>
-				</p>
-
-				<Link
-					to={{ type: LINK_FEED_LIKED_BY, actor: creator.did, rkey: getRecordId(feed.uri) }}
-					class="text-left text-sm text-muted-fg hover:underline"
-				>
-					Liked by {formatCompact(feed.likeCount.value)} users
-				</Link>
-
-				<div class="flex gap-2">
-					<button
-						title={!isLiked() ? `Like this feed` : `Unlike this feed`}
-						onClick={() => {
-							updateFeedLike(feed, !isLiked());
-						}}
-						class={/* @once */ Button({ variant: 'outline' })}
-					>
-						{isLiked() ? (
-							<FavoriteIcon class="-mx-1.5 text-base text-red-500" />
-						) : (
-							<FavoriteOutlinedIcon class="-mx-1.5 text-base" />
-						)}
-					</button>
-
-					<div class="grow"></div>
-
-					<FeedOverflowAction feed={feed}>
-						<button title="Actions" class={/* @once */ Button({ variant: 'outline' })}>
-							<MoreHorizIcon class="-mx-1.5 text-base" />
-						</button>
-					</FeedOverflowAction>
-				</div>
-			</div>
+			</VirtualContainer>
 		);
 	}) as unknown as JSX.Element;
 };
