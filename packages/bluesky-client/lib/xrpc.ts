8ac147db58fcb3efbc88ab34311a8e5e94acfca9
Mary <git@mary.my.id>
2024-02-24 16:00:08 +0700

    fix: do not wrap fetch error in xrpcerror

--- packages/bluesky-client/lib/xrpc.ts
+++ packages/bluesky-client/lib/xrpc.ts
@@ -44,28 +44,24 @@ export const defaultFetchHandler = async (
 	httpReqBody: unknown,
 	signal: AbortSignal | undefined,
 ): Promise<FetchHandlerResponse> => {
-	try {
-		// The duplex field is now required for streaming bodies, but not yet reflected
-		// anywhere in docs or types. See whatwg/fetch#1438, nodejs/node#46221.
-		const reqInit: RequestInit & { duplex: string } = {
-			signal: signal,
-			method: httpMethod,
-			headers: httpHeaders,
-			body: encodeMethodCallBody(httpHeaders, httpReqBody),
-			duplex: 'half',
-		};
+	// The duplex field is now required for streaming bodies, but not yet reflected
+	// anywhere in docs or types. See whatwg/fetch#1438, nodejs/node#46221.
+	const reqInit: RequestInit & { duplex: string } = {
+		signal: signal,
+		method: httpMethod,
+		headers: httpHeaders,
+		body: encodeMethodCallBody(httpHeaders, httpReqBody),
+		duplex: 'half',
+	};
 
-		const res = await fetch(httpUri, reqInit);
-		const resBody = await res.arrayBuffer();
+	const res = await fetch(httpUri, reqInit);
+	const resBody = await res.arrayBuffer();
 
-		return {
-			status: res.status,
-			headers: Object.fromEntries(res.headers.entries()),
-			body: httpResponseBodyParse(res.headers.get('content-type'), resBody),
-		};
-	} catch (e) {
-		throw new XRPCError(ResponseType.Unknown, String(e));
-	}
+	return {
+		status: res.status,
+		headers: Object.fromEntries(res.headers.entries()),
+		body: httpResponseBodyParse(res.headers.get('content-type'), resBody),
+	};
 };
 
 interface BaseRPCOptions {
