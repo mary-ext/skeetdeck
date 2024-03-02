0f4f66c3f87118418928cf9f7ffe863c34718d5d
Mary <git@mary.my.id>
2024-03-02 21:46:58 +0700

    refactor: not sure why i went for that approach

--- app/api/utils/cbor.ts
+++ app/api/utils/cbor.ts
@@ -2,8 +2,6 @@ import * as cborg from 'cborg';
 
 import { assert } from '~/utils/misc';
 
-import type { At } from '../atp-schema';
-
 import * as base32 from './base32';
 
 const decodeCidToBytes = (source: string) => {
@@ -13,8 +11,6 @@ const decodeCidToBytes = (source: string) => {
 	const str = source.slice(1);
 	return base32.decode(str);
 };
-
-const cidMap = new WeakMap<WeakKey, cborg.Token[]>();
 
 const CBORG_ENCODE_OPTIONS: cborg.EncodeOptions = {
 	float64: true,
@@ -33,17 +29,14 @@ const CBORG_ENCODE_OPTIONS: cborg.EncodeOptions = {
 			throw new Error(`undefined values not supported`);
 		},
 		Object: (val) => {
-			if ('$type' in val && val.$type === 'blob' && 'ref' in val) {
-				const blob = val as At.Blob;
-				const cid = decodeCidToBytes(blob.ref.$link);
+			if ('$link' in val && typeof val.$link === 'string') {
+				const cid = decodeCidToBytes(val.$link);
 
 				// CID bytes are prefixed with 0x00 for historical reasons, apparently.
 				const bytes = new Uint8Array(cid.byteLength + 1);
 				bytes.set(cid, 1);
 
-				cidMap.set(blob.ref, [new cborg.Token(cborg.Type.tag, 42), new cborg.Token(cborg.Type.bytes, bytes)]);
-			} else if (cidMap.has(val)) {
-				return cidMap.get(val)!;
+				return [new cborg.Token(cborg.Type.tag, 42), new cborg.Token(cborg.Type.bytes, bytes)];
 			}
 
 			return null;
