8205464f880107bf5eb8bf859d4aadaed38e9ca9
Mary <git@mary.my.id>
2024-03-18 13:05:17 +0700

    refactor: move mobile router definition

--- app/mobile/main.tsx
+++ app/mobile/main.tsx
@@ -1,7 +1,7 @@
 import { createRenderEffect, lazy } from 'solid-js';
 import { render } from 'solid-js/web';
 
-import { RouterView } from '@pkg/solid-navigation';
+import { RouterView, configureRouter } from '@pkg/solid-navigation';
 import { QueryClientProvider } from '@pkg/solid-query';
 
 import { MetaProvider } from '~/com/lib/meta';
@@ -11,8 +11,6 @@ import { useMediaQuery } from '~/utils/media-query';
 
 import { preferences } from './globals/settings';
 import { queryClient } from './globals/query';
-
-import './globals/router';
 
 import CircularProgress from '~/com/components/CircularProgress';
 
@@ -55,5 +53,93 @@ const App = () => {
 		</QueryClientProvider>
 	);
 };
+
+// Set up router
+{
+	const DID_RE = /^did:[a-z]+:[a-zA-Z0-9._\-]+$/;
+	// const HANDLE_RE = /^@([a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)*(?:\.[a-zA-Z]{2,}))$/;
+
+	const TID_RE = /^[234567abcdefghij][234567abcdefghijklmnopqrstuvwxyz]{12}$/;
+
+	const isValidDid = (did: string) => {
+		return DID_RE.test(did);
+	};
+	// const isValidHandle = (handleOrDid: string) => {
+	// 	return HANDLE_RE.test(handleOrDid);
+	// };
+	const isValidTid = (tid: string) => {
+		return tid.length === 13 && TID_RE.test(tid);
+	};
+
+	configureRouter({
+		routes: [
+			{
+				path: '/',
+				component: lazy(() => import('./views/LoggedOut')),
+				meta: {
+					public: true,
+				},
+			},
+			{
+				path: '/sign_in',
+				component: lazy(() => import('./views/SignIn')),
+				meta: {
+					public: true,
+				},
+			},
+
+			{
+				path: '/home',
+				component: lazy(() => import('./views/Home')),
+				single: true,
+				meta: {
+					name: 'Home',
+					main: true,
+				},
+			},
+			{
+				path: '/explore',
+				component: lazy(() => import('./views/Explore')),
+				single: true,
+				meta: {
+					name: 'Explore',
+					main: true,
+				},
+			},
+			{
+				path: '/notifications',
+				component: lazy(() => import('./views/Notifications')),
+				single: true,
+				meta: {
+					name: 'Notifications',
+					main: true,
+				},
+			},
+
+			{
+				path: '/:actor',
+				component: lazy(() => import('./views/Profile')),
+				validate: (params) => {
+					return isValidDid(params.actor);
+				},
+			},
+			{
+				path: '/:actor/:post',
+				component: lazy(() => import('./views/Thread')),
+				validate: (params) => {
+					return isValidDid(params.actor) && isValidTid(params.post);
+				},
+			},
+			{
+				path: '*',
+				component: lazy(() => import('./views/NotFound')),
+				meta: {
+					name: 'NotFound',
+					public: true,
+				},
+			},
+		],
+	});
+}
 
 render(() => <App />, document.body);
