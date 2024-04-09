d6e0ef1a3069e8c780cbabbfa0523d0d4adbd8c9
Mary <git@mary.my.id>
2024-04-09 13:17:39 +0700

    fix: remove undefined from return type

--- app/api/moderation/utils.ts
+++ app/api/moderation/utils.ts
@@ -4,10 +4,10 @@ import { type ModerationOptions, ContextProfileList, getModerationUI } from '.';
 import { moderateProfile } from './entities/profile';
 
 export const moderateProfileList = (
-	profiles: SignalizedProfile[] | undefined,
+	profiles: SignalizedProfile[],
 	opts: ModerationOptions | undefined,
-): SignalizedProfile[] | undefined => {
-	if (opts && profiles) {
+): SignalizedProfile[] => {
+	if (opts) {
 		return profiles.filter((profile) => {
 			const causes = moderateProfile(profile, opts);
 			const ui = getModerationUI(causes, ContextProfileList);
