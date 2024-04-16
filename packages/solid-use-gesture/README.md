77d84d23d5f6d5147eadb1443ce872459e2ddf1c
Mary <git@mary.my.id>
2024-04-16 15:22:58 +0700

    chore: remove unused use-gesture package

--- packages/solid-use-gesture/README.md
+++
@@ -1,3 +0,0 @@
-# solid-freeze
-
-Suspend rendering on Solid.js component subtrees
--- packages/solid-use-gesture/lib/types.ts
+++
@@ -1,9 +0,0 @@
-import type { JSX } from 'solid-js';
-
-import type { DOMHandlers } from '@use-gesture/core/types';
-
-type CombinedDOMHandlers = JSX.DOMAttributes<EventTarget> & DOMHandlers;
-
-export type SolidDOMAttributes = {
-	[Key in keyof DOMHandlers]: CombinedDOMHandlers[Key];
-};
