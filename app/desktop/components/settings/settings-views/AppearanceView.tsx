7cc60458ebb7bb0ef94a5279a89306231032e0c3
Mary <git@mary.my.id>
2024-04-22 11:07:51 +0700

    refactor: redo appearance view

--- app/desktop/components/settings/settings-views/AppearanceView.tsx
+++ app/desktop/components/settings/settings-views/AppearanceView.tsx
@@ -6,6 +6,13 @@ import { preferences } from '~/desktop/globals/settings';
 
 import Radio from '~/com/components/inputs/Radio';
 import Checkbox from '~/com/components/inputs/Checkbox';
+import {
+	ListBox,
+	ListBoxItemReadonly,
+	ListGroup,
+	ListGroupBlurb,
+	ListGroupHeader,
+} from '~/com/primitives/list-box';
 
 const AppearanceView = () => {
 	const ui = preferences.ui;
@@ -27,73 +34,78 @@ const AppearanceView = () => {
 			<div class="flex h-13 shrink-0 items-center gap-2 border-b border-divider px-4">
 				<h2 class="grow text-base font-bold">Appearance</h2>
 			</div>
-			<div class="grow overflow-y-auto">
-				<div class="flex flex-col gap-3 px-4 py-3 text-sm">
-					<p class="font-medium leading-6 text-primary">Theme</p>
+			<div class="flex grow flex-col gap-6 overflow-y-auto p-4">
+				<div class={ListGroup}>
+					<p class={ListGroupHeader}>Application theme</p>
 
-					<label class="flex items-center justify-between gap-2">
-						<span>Automatic</span>
-						<Radio ref={themeModel('auto')} name={themeId} />
-					</label>
-					<label class="flex items-center justify-between gap-2">
-						<span>Light</span>
-						<Radio ref={themeModel('light')} name={themeId} />
-					</label>
-					<label class="flex items-center justify-between gap-2">
-						<span>Dark</span>
-						<Radio ref={themeModel('dark')} name={themeId} />
-					</label>
+					<div class={ListBox}>
+						<label class={ListBoxItemReadonly}>
+							<span class="grow font-medium">Automatic</span>
+							<Radio ref={themeModel('auto')} name={themeId} />
+						</label>
+						<label class={ListBoxItemReadonly}>
+							<span class="grow font-medium">Light</span>
+							<Radio ref={themeModel('light')} name={themeId} />
+						</label>
+						<label class={ListBoxItemReadonly}>
+							<span class="grow font-medium">Dark</span>
+							<Radio ref={themeModel('dark')} name={themeId} />
+						</label>
+					</div>
 				</div>
 
-				<div class="flex flex-col gap-3 px-4 py-3 text-sm">
-					<p class="font-medium leading-6 text-primary">Default column size</p>
+				<div class={ListGroup}>
+					<p class={ListGroupHeader}>Default column size</p>
 
-					<label class="flex items-center justify-between gap-2">
-						<span>Small</span>
-						<Radio ref={paneSizeModel(PaneSize.SMALL)} name={columnSizeId} />
-					</label>
-					<label class="flex items-center justify-between gap-2">
-						<span>Medium</span>
-						<Radio ref={paneSizeModel(PaneSize.MEDIUM)} name={columnSizeId} />
-					</label>
-					<label class="flex items-center justify-between gap-2">
-						<span>Large</span>
-						<Radio ref={paneSizeModel(PaneSize.LARGE)} name={columnSizeId} />
-					</label>
+					<div class={ListBox}>
+						<label class={ListBoxItemReadonly}>
+							<span class="grow font-medium">Small</span>
+							<Radio ref={paneSizeModel(PaneSize.SMALL)} name={columnSizeId} />
+						</label>
+						<label class={ListBoxItemReadonly}>
+							<span class="grow font-medium">Medium</span>
+							<Radio ref={paneSizeModel(PaneSize.MEDIUM)} name={columnSizeId} />
+						</label>
+						<label class={ListBoxItemReadonly}>
+							<span class="grow font-medium">Large</span>
+							<Radio ref={paneSizeModel(PaneSize.LARGE)} name={columnSizeId} />
+						</label>
+					</div>
 				</div>
 
-				<hr class="mx-4 mb-2 mt-1 border-divider" />
+				<div class={ListGroup}>
+					<p class={ListGroupHeader}>Appearance</p>
 
-				<div class="px-4 py-3">
-					<label class="flex min-w-0 justify-between gap-4">
-						<span class="text-sm">Show profile media in grid form</span>
+					<div class={ListBox}>
+						<label class={ListBoxItemReadonly}>
+							<div class="grow">
+								<p class="grow font-medium">Show profile media in grid form</p>
+								<p class="text-de text-muted-fg">This will not affect feeds/lists panes</p>
+							</div>
+							<Checkbox
+								checked={ui.profileMediaGrid}
+								onChange={(ev) => {
+									const next = ev.target.checked;
+									ui.profileMediaGrid = next;
+								}}
+							/>
+						</label>
 
-						<Checkbox
-							checked={ui.profileMediaGrid}
-							onChange={(ev) => {
-								const next = ev.target.checked;
-								ui.profileMediaGrid = next;
-							}}
-						/>
-					</label>
+						<label class={ListBoxItemReadonly}>
+							<div class="grow">
+								<p class="font-medium">Show thread replies in threaded form</p>
+								<p class="text-de text-muted-fg">This is an experimental feature</p>
+							</div>
 
-					<p class="mr-6 text-de text-muted-fg">This preference will not affect feeds/lists panes</p>
-				</div>
-
-				<div class="px-4 py-3">
-					<label class="flex min-w-0 justify-between gap-4">
-						<span class="text-sm">Show thread replies in threaded form</span>
-
-						<Checkbox
-							checked={ui.threadedReplies}
-							onChange={(ev) => {
-								const next = ev.target.checked;
-								ui.threadedReplies = next;
-							}}
-						/>
-					</label>
-
-					<p class="mr-6 text-de text-muted-fg">This is an experimental feature.</p>
+							<Checkbox
+								checked={ui.threadedReplies}
+								onChange={(ev) => {
+									const next = ev.target.checked;
+									ui.threadedReplies = next;
+								}}
+							/>
+						</label>
+					</div>
 				</div>
 			</div>
 		</div>
