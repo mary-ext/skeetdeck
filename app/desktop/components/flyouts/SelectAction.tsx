00f05b7178be5b4325e7392befbf9fc7b4dfad9d
Mary <git@mary.my.id>
2024-04-23 17:26:23 +0700

    refactor: new language veiw

---
+++ app/desktop/components/flyouts/SelectAction.tsx
@@ -0,0 +1,121 @@
+import { onMount, untrack, type JSX } from 'solid-js';
+
+import { clsx } from '~/utils/misc';
+
+import { MenuItem, MenuRoot } from '~/com/primitives/menu';
+
+import { Flyout, offsetlessMiddlewares } from '~/com/components/Flyout';
+
+import CheckIcon from '~/com/icons/baseline-check';
+
+export interface SelectItem<T> {
+	value: T;
+	label: JSX.Element;
+}
+
+export interface SelectActionProps<T> {
+	value?: T;
+	options: SelectItem<T>[];
+	onChange: (next: T) => void;
+	children: JSX.Element;
+}
+
+const SelectAction = <T,>(props: SelectActionProps<T>) => {
+	return (
+		<Flyout button={props.children} middleware={offsetlessMiddlewares} placement="bottom-end">
+			{({ close, menuProps }) => {
+				const onChange = props.onChange;
+
+				return (() => {
+					let selected: HTMLButtonElement | undefined;
+
+					const options = props.options;
+					const initialValue = untrack(() => props.value);
+
+					const handleKeyDown = (ev: KeyboardEvent) => {
+						const key = ev.key;
+
+						const isArrowUp = key === 'ArrowUp';
+						const isArrowDown = key === 'ArrowDown';
+
+						if (!selected || (!isArrowUp && !isArrowDown)) {
+							return;
+						}
+
+						const next = isArrowUp ? selected.previousSibling : selected.nextSibling;
+
+						if (!(next instanceof HTMLButtonElement)) {
+							return;
+						}
+
+						ev.preventDefault();
+
+						next.tabIndex = 0;
+						next.focus();
+						next.scrollIntoView({ block: 'center' });
+
+						selected.tabIndex = -1;
+						selected = next;
+					};
+
+					return (
+						<div
+							ref={(node) => {
+								menuProps.ref(node);
+
+								onMount(() => {
+									if (selected) {
+										selected.focus();
+
+										// blergh
+										requestAnimationFrame(() => {
+											selected!.scrollIntoView({ block: 'center' });
+										});
+									} else {
+										const first = node.querySelector<HTMLButtonElement>(':scope > button');
+
+										if (first) {
+											first.focus();
+											selected = first;
+										}
+									}
+								});
+							}}
+							onKeyDown={handleKeyDown}
+							class={/* @once */ MenuRoot()}
+							style={menuProps.style}
+						>
+							{options.map(({ value, label }) => {
+								return (
+									<button
+										ref={(node) => {
+											if (value === initialValue) {
+												node.tabIndex = 0;
+												selected = node;
+											} else {
+												node.tabIndex = -1;
+											}
+										}}
+										onClick={() => {
+											close();
+											onChange(value);
+										}}
+										class={/* @once */ MenuItem()}
+									>
+										<span class="min-w-0 grow">{label}</span>
+
+										<CheckIcon
+											class={clsx([`shrink-0 text-base text-accent`, value !== props.value && `invisible`])}
+										/>
+									</button>
+								);
+							})}
+						</div>
+					);
+				}) as unknown as JSX.Element;
+			}}
+		</Flyout>
+	);
+};
+
+export default SelectAction;
--- app/desktop/components/settings/settings-views/LanguageView.tsx
+++ app/desktop/components/settings/settings-views/LanguageView.tsx
@@ -2,38 +2,48 @@ import { systemLanguages } from '~/api/globals/platform';
 
 import { getNativeLanguageName, languageNames, languageNamesStrict } from '~/utils/intl/display-names';
 import { CODE2S } from '~/utils/intl/languages';
+import { modelChecked } from '~/utils/input';
 import { mapDefined } from '~/utils/misc';
 
 import { preferences } from '~/desktop/globals/settings';
 
-import { Interactive } from '~/com/primitives/interactive';
+import {
+	ListBox,
+	ListBoxItemChevron,
+	ListBoxItemInteractive,
+	ListBoxItemReadonly,
+	ListGroup,
+	ListGroupBlurb,
+	ListGroupHeader,
+} from '~/com/primitives/list-box';
 
 import Checkbox from '~/com/components/inputs/Checkbox';
-import SelectInput, { type SelectItem } from '~/com/components/inputs/SelectInput';
 
+import ArrowDropDownIcon from '~/com/icons/baseline-arrow-drop-down';
 import ChevronRightIcon from '~/com/icons/baseline-chevron-right';
 
 import { VIEW_ADDITIONAL_LANGUAGE, VIEW_EXCLUDED_TRANSLATION, useViewRouter } from './_router';
 
-const selectItem = Interactive({
-	class: `flex items-center justify-between gap-4 px-4 py-3 text-left text-sm`,
-});
+import type { SelectItem } from '../../flyouts/SelectAction';
+import SelectAction from '../../flyouts/SelectAction';
 
-const LanguageView = () => {
-	const router = useViewRouter();
-
-	const langs = preferences.language;
-	const trans = preferences.translation;
-
-	const availableLanguages: SelectItem[] = [
+const getAvailableLanguages = (isTranslate: boolean) => {
+	const availableLanguages: SelectItem<string>[] = [
 		{
 			value: 'none',
-			label: 'None',
+			label: !isTranslate ? `None` : `Disabled`,
 		},
 		{
 			value: 'system',
 			get label() {
-				return `Primary system language (${languageNames.of(systemLanguages[0])})`;
+				const lang = languageNames.of(systemLanguages[0]);
+
+				return (
+					<>
+						<p>System default</p>
+						<p class="text-de text-muted-fg">{lang}</p>
+					</>
+				);
 			},
 		},
 		...mapDefined(CODE2S, (code) => {
@@ -46,132 +56,191 @@ const LanguageView = () => {
 
 			return {
 				value: code,
-				label: `${eng}${native !== eng ? ` - ${native}` : ``}`,
+				get label() {
+					return (
+						<>
+							<p>{eng}</p>
+							<p class="text-de text-muted-fg">{native}</p>
+						</>
+					);
+				},
 			};
 		}),
 	];
+
+	return availableLanguages;
+};
+
+const LanguageView = () => {
+	const router = useViewRouter();
+
+	const langs = preferences.language;
+	const trans = preferences.translation;
+
+	const getLanguageLabel = (code: string, isTranslate: boolean) => {
+		if (code === 'none') {
+			return !isTranslate ? 'None' : `Disabled`;
+		}
+
+		if (code === 'system') {
+			return `System default (${languageNames.of(systemLanguages[0])})`;
+		}
+
+		const eng = languageNamesStrict.of(code);
+
+		if (!eng) {
+			return `Unknown (${code})`;
+		}
+
+		return eng;
+	};
 
 	return (
 		<div class="contents">
 			<div class="flex h-13 shrink-0 items-center gap-2 border-b border-divider px-4">
 				<h2 class="grow text-base font-bold">Language</h2>
 			</div>
-			<div class="flex grow flex-col overflow-y-auto pb-4">
-				<div class="px-4 py-3">
-					<label class="flex flex-col gap-2">
-						<span class="text-sm font-medium leading-6 text-primary">Post language</span>
+			<div class="flex grow flex-col gap-6 overflow-y-auto p-4">
+				<div class={ListGroup}>
+					<p class={ListGroupHeader}>Content you post</p>
 
-						<SelectInput
+					<div class={ListBox}>
+						<SelectAction
 							value={langs.defaultPostLanguage}
-							options={availableLanguages}
-							onChange={(next) => {
-								langs.defaultPostLanguage = next;
-							}}
-						/>
-					</label>
+							options={getAvailableLanguages(false)}
+							onChange={(next) => (langs.defaultPostLanguage = next)}
+						>
+							<button class={ListBoxItemInteractive}>
+								<div class="flex min-w-0 grow flex-col">
+									<div class="flex justify-between gap-3">
+										<span class="grow font-medium">Post language</span>
 
-					<p class="mt-2 text-de text-muted-fg">
-						This is the language used when creating new posts, it will not affect your existing posts.
-					</p>
+										<span class="flex min-w-0 shrink-0 items-center gap-0.5 self-start text-muted-fg">
+											<span class="text-de">{getLanguageLabel(langs.defaultPostLanguage, false)}</span>
+											<ArrowDropDownIcon class="-mr-1 text-base" />
+										</span>
+									</div>
+
+									<p class="mt-1 text-de text-muted-fg">
+										The default language used when creating new posts, it will not affect your existing posts
+									</p>
+								</div>
+							</button>
+						</SelectAction>
+					</div>
 				</div>
 
-				<hr class="mx-4 mt-1 border-divider" />
+				<div class={ListGroup}>
+					<p class={ListGroupHeader}>Content you see</p>
 
-				<p class="p-4 text-base font-bold leading-5">Content languages</p>
+					<div class={ListBox}>
+						<label class={ListBoxItemReadonly}>
+							<div class="flex min-w-0 grow flex-col">
+								<div class="flex justify-between gap-3">
+									<span class="grow font-medium">Use my system's languages</span>
+									<Checkbox
+										ref={modelChecked(
+											() => langs.useSystemLanguages,
+											(next) => (langs.useSystemLanguages = next),
+										)}
+									/>
+								</div>
 
-				<p class="px-4 pb-3 text-de text-muted-fg">
-					Controls which languages appears in your feeds, this does not apply to your following feed. Removing
-					all languages reveals posts from all languages.
-				</p>
+								<p class="mt-1 text-de text-muted-fg">
+									Show posts that matches your browser's language preferences
+								</p>
+							</div>
+						</label>
 
-				<div class="px-4 py-3">
-					<label class="flex min-w-0 justify-between gap-4">
-						<span class="text-sm">Show unspecified posts</span>
+						<button
+							onClick={() => router.move({ type: VIEW_ADDITIONAL_LANGUAGE })}
+							class={ListBoxItemInteractive}
+						>
+							<div class="grow">
+								<p class="font-medium">Additional languages</p>
+								<p class="text-de text-muted-fg">
+									{(() => {
+										const count = langs.languages.length;
 
-						<Checkbox
-							checked={langs.allowUnspecified}
-							onChange={(ev) => {
-								const next = ev.target.checked;
-								langs.allowUnspecified = next;
-							}}
-						/>
-					</label>
+										if (count === 1) {
+											return `${count} language added`;
+										}
 
-					<p class="mr-6 text-de text-muted-fg">Do not filter posts that does not specify a language.</p>
-				</div>
+										return `${count} languages added`;
+									})()}
+								</p>
+							</div>
 
-				<div class="px-4 py-3">
-					<label class="flex justify-between gap-4">
-						<span class="text-sm">Use system languages</span>
-						<Checkbox
-							checked={langs.useSystemLanguages}
-							onChange={(ev) => {
-								const next = ev.target.checked;
-								langs.useSystemLanguages = next;
-							}}
-						/>
-					</label>
+							<ChevronRightIcon class={ListBoxItemChevron} />
+						</button>
 
-					<p class="mr-6 text-de text-muted-fg">
-						Use the languages detected from your system or browser preferences
-					</p>
-				</div>
+						<label class={ListBoxItemReadonly}>
+							<div class="flex min-w-0 grow flex-col">
+								<div class="flex justify-between gap-3">
+									<span class="grow font-medium">Don't show undeclared posts</span>
+									<Checkbox
+										ref={modelChecked(
+											() => !langs.allowUnspecified,
+											(next) => (langs.allowUnspecified = !next),
+										)}
+									/>
+								</div>
 
-				<button onClick={() => router.move({ type: VIEW_ADDITIONAL_LANGUAGE })} class={selectItem}>
-					<div>
-						<p>Additional content languages</p>
-						<p class="text-de text-muted-fg">
-							{(() => {
-								const count = langs.languages.length;
-
-								if (count === 1) {
-									return `${count} language added`;
-								}
-
-								return `${count} languages added`;
-							})()}
-						</p>
+								<p class="mt-1 text-de text-muted-fg">
+									Hide posts that has not declared what language it is in
+								</p>
+							</div>
+						</label>
 					</div>
 
-					<ChevronRightIcon class="text-xl text-muted-fg" />
-				</button>
+					<p class={ListGroupBlurb}>
+						Adjusts the content you see on Bluesky. Removing all languages will result in all posts being
+						shown.
+					</p>
+				</div>
 
-				<hr class="mx-4 mt-1 border-divider" />
+				<div class={ListGroup}>
+					<p class={ListGroupHeader}>Content translation</p>
 
-				<p class="p-4 text-base font-bold leading-5">Content translation</p>
-
-				<div class="px-4 py-3">
-					<label class="flex flex-col gap-2">
-						<span class="text-sm font-medium leading-6 text-primary">Translate into this language</span>
-
-						<SelectInput
+					<div class={ListBox}>
+						<SelectAction
 							value={trans.to}
-							options={availableLanguages}
-							onChange={(next) => {
-								trans.to = next;
-							}}
-						/>
-					</label>
-				</div>
+							options={getAvailableLanguages(true)}
+							onChange={(next) => (trans.to = next)}
+						>
+							<button class={ListBoxItemInteractive}>
+								<span class="grow font-medium">Translate to</span>
 
-				<button onClick={() => router.move({ type: VIEW_EXCLUDED_TRANSLATION })} class={selectItem}>
-					<div>
-						<p>Exclude languages from translation</p>
-						<p class="text-de text-muted-fg">
-							{(() => {
-								const count = trans.exclusions.length;
+								<span class="flex min-w-0 shrink-0 items-center gap-0.5 self-start text-muted-fg">
+									<span class="text-de">{getLanguageLabel(trans.to, true)}</span>
+									<ArrowDropDownIcon class="-mr-1 text-base" />
+								</span>
+							</button>
+						</SelectAction>
 
-								if (count === 1) {
-									return `${count} language excluded`;
-								}
+						<button
+							onClick={() => router.move({ type: VIEW_EXCLUDED_TRANSLATION })}
+							class={ListBoxItemInteractive}
+						>
+							<div class="grow">
+								<p class="font-medium">Exclude languages from translation</p>
+								<p class="text-de text-muted-fg">
+									{(() => {
+										const count = trans.exclusions.length;
 
-								return `${count} languages excluded`;
-							})()}
-						</p>
+										if (count === 1) {
+											return `${count} language excluded`;
+										}
+
+										return `${count} languages excluded`;
+									})()}
+								</p>
+							</div>
+
+							<ChevronRightIcon class={ListBoxItemChevron} />
+						</button>
 					</div>
-
-					<ChevronRightIcon class="text-xl text-muted-fg" />
-				</button>
+				</div>
 			</div>
 		</div>
 	);
