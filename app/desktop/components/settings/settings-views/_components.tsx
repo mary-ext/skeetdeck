2ffda9bc3b3c3c36a03c736629f7b8623e621aae
Mary <git@mary.my.id>
2024-04-24 09:20:53 +0700

    fix: type=button

--- app/desktop/components/settings/settings-views/_components.tsx
+++ app/desktop/components/settings/settings-views/_components.tsx
@@ -16,7 +16,7 @@ export interface DropdownItemProps<T> extends Omit<SelectActionProps<T>, 'childr
 export const SelectionItem = <T,>(props: DropdownItemProps<T>) => {
 	return (
 		<SelectAction value={props.value} options={props.options} onChange={props.onChange}>
-			<button class={ListBoxItemInteractive}>
+			<button type="button" class={ListBoxItemInteractive}>
 				<div class="flex min-w-0 grow flex-col text-sm">
 					<div class="flex justify-between gap-3">
 						<span class="overflow-hidden text-ellipsis whitespace-nowrap font-medium">{props.title}</span>
