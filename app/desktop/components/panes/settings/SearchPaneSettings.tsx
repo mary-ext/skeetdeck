913dbf369d4abb241f0aeeae00b2b4fbc208fb62
Mary <git@mary.my.id>
2024-04-25 15:55:16 +0700

    feat: configure order by

--- app/desktop/components/panes/settings/SearchPaneSettings.tsx
+++ app/desktop/components/panes/settings/SearchPaneSettings.tsx
@@ -1,8 +1,12 @@
 import TextareaAutoresize from 'solid-textarea-autosize';
 
+import { getUniqueId } from '~/utils/misc';
+
 import type { SearchPaneConfig } from '../../../globals/panes';
 
 import { Textarea } from '~/com/primitives/textarea';
+
+import Radio from '~/com/components/inputs/Radio';
 
 import SearchIcon from '~/com/icons/baseline-search';
 
@@ -11,31 +15,51 @@ import { usePaneContext } from '../PaneContext';
 const SearchPaneSettings = () => {
 	const { pane } = usePaneContext<SearchPaneConfig>();
 
+	const id = getUniqueId();
+
 	return (
-		<div class="border-b border-divider p-4">
-			<div class="relative grow">
-				<div class="pointer-events-none absolute inset-y-0 ml-px grid place-items-center px-2">
-					<SearchIcon class="text-lg text-muted-fg" />
-				</div>
-				<TextareaAutoresize
-					class={/* @once */ Textarea({ class: 'pl-8' })}
-					value={pane.query}
-					placeholder="Search..."
-					minRows={1}
-					onKeyDown={(ev) => {
-						if (ev.key === 'Enter') {
-							const value = ev.currentTarget.value.trim();
+		<div class="contents">
+			<div class="border-b border-divider p-4">
+				<div class="relative grow">
+					<div class="pointer-events-none absolute inset-y-0 ml-px grid place-items-center px-2">
+						<SearchIcon class="text-lg text-muted-fg" />
+					</div>
+					<TextareaAutoresize
+						class={/* @once */ Textarea({ class: 'pl-8' })}
+						value={pane.query}
+						placeholder="Search..."
+						minRows={1}
+						onKeyDown={(ev) => {
+							if (ev.key === 'Enter') {
+								const value = ev.currentTarget.value.trim();
 
-							if (value) {
-								pane.query = value;
-							} else {
-								ev.currentTarget.value = pane.query;
+								if (value) {
+									pane.query = value;
+								} else {
+									ev.currentTarget.value = pane.query;
+								}
+
+								ev.preventDefault();
 							}
+						}}
+					/>
+				</div>
+			</div>
 
-							ev.preventDefault();
-						}
-					}}
-				/>
+			<div class="flex flex-col border-b border-divider pb-5">
+				<p class="p-4 text-sm font-bold">Order by</p>
+
+				<div class="mx-4 flex flex-col gap-2 text-sm">
+					<label class="flex items-center justify-between gap-2">
+						<span>Latest</span>
+						<Radio name={id} checked={pane.sort === 'latest'} onChange={() => (pane.sort = 'latest')} />
+					</label>
+
+					<label class="flex items-center justify-between gap-2">
+						<span>Top</span>
+						<Radio name={id} checked={pane.sort === 'top'} onChange={() => (pane.sort = 'top')} />
+					</label>
+				</div>
 			</div>
 		</div>
 	);
