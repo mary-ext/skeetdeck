c8fcda2c4575ef6516a4631c3f6fe54fb59f4455
Mary <git@mary.my.id>
2024-04-26 15:13:30 +0700

    feat: youtube iframe support

--- app/com/components/embeds/EmbedLink.tsx
+++ app/com/components/embeds/EmbedLink.tsx
@@ -9,18 +9,13 @@ import PlayIcon from '../../icons/baseline-play';
 import BlobImage from '../BlobImage';
 import CircularProgress from '../CircularProgress';
 
-interface TenorData {
-	u: string;
-	r: string;
-	d: string;
-}
+import { type Snippet, SnippetType, detectSnippet } from '~/com/lib/snippet';
 
 type EmbeddedLink = AppBskyEmbedExternal.ViewExternal;
 
 export interface EmbedLinkData extends Omit<EmbeddedLink, 'thumb'> {
 	thumb?: Blob | string;
-	_t?: TenorData | null;
-	_d?: string;
+	_s?: Snippet;
 }
 
 export interface EmbedLinkProps {
@@ -28,40 +23,28 @@ export interface EmbedLinkProps {
 	interactive?: boolean;
 }
 
-const embedLinkInteractive = Interactive({ variant: 'muted', class: `w-full rounded-md` });
-const BSKY_TENOR_RE = /^https:\/\/media\.tenor\.com\/([^/]+?AAAAC)\/([^\/]+?)\?hh=(\d+?)&ww=(\d+?)$/;
-
 const EmbedLink = (props: EmbedLinkProps) => {
 	return (() => {
+		const interactive = props.interactive;
 		const link = props.link;
-		const { uri, thumb, title, description } = link;
 
-		let tenor = link._t;
-		let domain = link._d;
+		const thumb = link.thumb;
 
-		if (tenor === undefined) {
-			const HOST = 'https://t.gifs.bsky.app';
-
-			const match = BSKY_TENOR_RE.exec(uri);
-			const result: TenorData | null = match
-				? {
-						u: HOST + '/' + match[1].replace(/AAAAC$/, 'AAAP3') + '/' + match[2].replace(/\.gif$/, '.webm'),
-						r: `${match[4]}/${match[3]}`,
-						d: description.replace(/^ALT: /, ''),
-					}
-				: null;
-
-			tenor = link._t = result;
+		let snippet = link._s;
+		if (snippet === undefined) {
+			link._s = snippet = detectSnippet(link);
 		}
 
-		if (tenor) {
+		const t = snippet.t;
+
+		if (t === SnippetType.BLUESKY_GIF) {
 			const [playing, setPlaying] = createSignal(false);
 			const [stalling, setStalling] = createSignal(false);
 
 			return (
 				<div
 					class="relative overflow-hidden rounded-md border border-divider"
-					style={/* @once */ { 'aspect-ratio': tenor.r }}
+					style={/* @once */ { 'aspect-ratio': snippet.r }}
 				>
 					<video
 						tabindex={-1}
@@ -76,7 +59,7 @@ const EmbedLink = (props: EmbedLinkProps) => {
 							});
 						}}
 						loop
-						src={/* @once */ tenor.u}
+						src={/* @once */ snippet.u}
 						onWaiting={() => setStalling(true)}
 						onPlaying={() => setStalling(false)}
 						class="h-full w-full"
@@ -86,7 +69,7 @@ const EmbedLink = (props: EmbedLinkProps) => {
 
 					<button
 						title={!playing() ? 'Play GIF' : `Pause GIF`}
-						aria-description={/* @once */ tenor.d}
+						aria-description={/* @once */ snippet.d}
 						onClick={() => setPlaying(!playing())}
 						class="absolute inset-0 grid place-items-center rounded-md outline-2 -outline-offset-2 outline-white focus-visible:outline"
 					>
@@ -102,15 +85,57 @@ const EmbedLink = (props: EmbedLinkProps) => {
 			);
 		}
 
-		if (!domain) {
-			try {
-				const host = new URL(uri).host;
-				domain = host.startsWith('www.') ? host.slice(4) : host;
-			} catch {
-				domain = uri;
-			}
+		if (t === SnippetType.IFRAME && interactive) {
+			const [show, setShow] = createSignal(false);
 
-			link._d = domain;
+			return [
+				() => {
+					if (show()) {
+						return (
+							<div
+								class="overflow-hidden rounded-md border border-divider"
+								style={/* @once */ { 'aspect-ratio': snippet.r }}
+							>
+								<iframe src={/* @once */ snippet.u} allowfullscreen class="h-full w-full" />
+							</div>
+						);
+					}
+
+					return (
+						<div class="flex overflow-hidden rounded-md border border-divider">
+							<button
+								onClick={() => setShow(true)}
+								class="relative aspect-square w-[86px] shrink-0 border-r border-divider"
+							>
+								{thumb && <BlobImage src={thumb} class="h-full w-full object-cover" />}
+
+								<div class="absolute inset-0 grid place-items-center">
+									<div class="grid h-9 w-9 place-items-center rounded-full border-2 border-white bg-accent">
+										<PlayIcon class="text-2xl" />
+									</div>
+								</div>
+							</button>
+
+							<a
+								href={/* @once */ link.uri}
+								rel="noopener noreferrer nofollow"
+								target="_blank"
+								class={
+									/* @once */ Interactive({
+										variant: 'muted',
+										class: 'flex min-w-0 grow flex-col justify-center gap-0.5 p-3 text-sm',
+									})
+								}
+							>
+								<p class="overflow-hidden text-ellipsis text-muted-fg empty:hidden">
+									{/* @once */ snippet.d}
+								</p>
+								<p class="line-clamp-2 break-words empty:hidden">{/* @once */ link.title}</p>
+							</a>
+						</div>
+					);
+				},
+			];
 		}
 
 		const content = (
@@ -123,15 +148,20 @@ const EmbedLink = (props: EmbedLinkProps) => {
 				)}
 
 				<div class="flex min-w-0 flex-col justify-center gap-0.5 p-3 text-sm">
-					<p class="overflow-hidden text-ellipsis text-muted-fg">{domain}</p>
-					<p class="line-clamp-2 break-words empty:hidden">{title}</p>
+					<p class="overflow-hidden text-ellipsis text-muted-fg empty:hidden">{/* @once */ snippet.d}</p>
+					<p class="line-clamp-2 break-words empty:hidden">{/* @once */ link.title}</p>
 				</div>
 			</div>
 		);
 
-		if (props.interactive) {
+		if (interactive) {
 			return (
-				<a href={uri} rel="noopener noreferrer nofollow" target="_blank" class={embedLinkInteractive}>
+				<a
+					href={/* @once */ link.uri}
+					rel="noopener noreferrer nofollow"
+					target="_blank"
+					class={/* @once */ Interactive({ variant: 'muted', class: `w-full rounded-md` })}
+				>
 					{content}
 				</a>
 			);
---
+++ app/com/lib/snippet.ts
@@ -0,0 +1,129 @@
+import type { AppBskyEmbedExternal } from '@mary/bluesky-client/lexicons';
+
+export const enum SnippetType {
+	LINK,
+	BLUESKY_GIF,
+	IFRAME,
+}
+
+export const enum SnippetSource {
+	YOUTUBE,
+}
+
+interface LinkSnippet {
+	t: SnippetType.LINK;
+	/** Domain name */
+	d?: string;
+}
+
+interface BlueskyGifSnippet {
+	t: SnippetType.BLUESKY_GIF;
+	/** Video URL */
+	u: string;
+	/** Aspect ratio */
+	r: string;
+	/** Alt text description */
+	d: string;
+}
+
+interface IframeSnippet {
+	t: SnippetType.IFRAME;
+	/** Source type */
+	s: SnippetSource;
+	/** Source domain */
+	d: string;
+	/** Iframe URL */
+	u: string;
+	/** Aspect ratio */
+	r: string;
+}
+
+export type Snippet = LinkSnippet | BlueskyGifSnippet | IframeSnippet;
+
+export const detectSnippet = (
+	link: Pick<AppBskyEmbedExternal.ViewExternal, 'uri' | 'description'>,
+): Snippet => {
+	const url = link.uri;
+
+	let u: URL;
+	let m: RegExpExecArray | null | undefined;
+
+	try {
+		u = new URL(url);
+
+		if (u.protocol !== 'https:' && u.protocol !== 'http:') {
+			return { t: SnippetType.LINK };
+		}
+	} catch {
+		return { t: SnippetType.LINK };
+	}
+
+	const h = u.host;
+	const p = u.pathname;
+	const q = u.searchParams;
+
+	const d = h.startsWith('www.') ? h.slice(4) : h;
+
+	if (d === 'media.tenor.com') {
+		// Bluesky GIFs
+		if ((m = /\/([^/]+?AAAAC)\/([^/]+?)\?hh=(\d+?)&ww=(\d+?)$/.exec(url))) {
+			const id = m[1].replace(/AAAAC$/, 'AAAP3');
+			const file = m[2].replace(/\.gif$/, '.webm');
+
+			const width = m[4];
+			const height = m[3];
+
+			return {
+				t: SnippetType.BLUESKY_GIF,
+				u: `https://t.gifs.bsky.app/${id}/${file}`,
+				r: `${width}/${height}`,
+				d: link.description.replace(/^ALT: /, ''),
+			};
+		}
+	} else if (d === 'youtube.com' || d === 'm.youtube.com' || d === 'music.youtube.com') {
+		// YouTube iframe
+		if (p === '/watch') {
+			const videoId = q.get('v');
+			const seek = q.get('t') || 0;
+
+			return {
+				t: SnippetType.IFRAME,
+				s: SnippetSource.YOUTUBE,
+				d: d,
+				u: `https://www.youtube-nocookie.com/embed/${videoId}?start=${seek}&autoplay=1&playsinline=1`,
+				r: `16/9`,
+			};
+		} else if ((m = /^\/shorts\/([^/]+?)$/.exec(p))) {
+			const videoId = m[1];
+
+			return {
+				t: SnippetType.IFRAME,
+				s: SnippetSource.YOUTUBE,
+				d: d,
+				u: `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&playsinline=1`,
+				r: `9/16`,
+			};
+		}
+	} else if (d === 'youtu.be') {
+		// YouTube iframe
+		if ((m = /^\/([^/]+?)$/.exec(p))) {
+			const videoId = m[1];
+
+			return {
+				t: SnippetType.IFRAME,
+				s: SnippetSource.YOUTUBE,
+				d: d,
+				u: `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&playsinline=1`,
+				r: `16/9`,
+			};
+		}
+	}
+
+	{
+		// Link snippet, always matches
+		return {
+			t: SnippetType.LINK,
+			d: d,
+		};
+	}
+};
