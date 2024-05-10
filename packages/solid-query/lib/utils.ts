0203dc60ef265d63749b2c3c90be0a421c658c79
Mary <git@mary.my.id>
2024-05-10 14:41:22 +0700

    refactor: remove unused shouldThrowError

--- packages/solid-query/lib/utils.ts
+++ packages/solid-query/lib/utils.ts
@@ -1,17 +1,5 @@
 import { createSignal } from 'solid-js';
 
-export function shouldThrowError<T extends (...args: Array<any>) => boolean>(
-	throwError: boolean | T | undefined,
-	params: Parameters<T>,
-): boolean {
-	// Allow throwError function to override throwing behavior on a per-error basis
-	if (typeof throwError === 'function') {
-		return throwError(...params);
-	}
-
-	return !!throwError;
-}
-
 export const createStateObject = <T extends Record<string, any>>(obj: T): T => {
 	const state = {} as T;
 
