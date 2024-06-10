96fa99c4bbf01a7641d126f0604745cab2e8a692
Mary <git@mary.my.id>
2024-06-10 09:15:26 +0700

    fix: make serializing work again

--- app/api/richtext/utils.ts
+++ app/api/richtext/utils.ts
@@ -12,17 +12,26 @@ export const serializeRichText = (text: string, facets: Facet[] | undefined, loo
 
 	for (let idx = 0, len = segments.length; idx < len; idx++) {
 		const segment = segments[idx];
-
 		const text = segment.text;
-		const link = segment.link;
-		const tag = segment.tag;
-		const mention = segment.mention;
+		const feature = segment.feature;
 
-		if (link) {
-			const uri = link.uri;
-			result += isLinkValid(uri, text) ? uri : `[${text.replace(MDLINK_ESCAPE_RE, '\\$1')}](${uri})`;
-		} else if (loose || tag || mention) {
-			result += text;
+		let substitute: string | undefined;
+
+		if (feature) {
+			const type = feature.$type;
+
+			if (type === 'app.bsky.richtext.facet#link') {
+				const uri = feature.uri;
+				substitute = isLinkValid(uri, text) ? uri : `[${text.replace(MDLINK_ESCAPE_RE, '\\$1')}](${uri})`;
+			} else if (type === 'app.bsky.richtext.facet#mention' || type === 'app.bsky.richtext.facet#tag') {
+				substitute = text;
+			}
+		} else if (loose) {
+			substitute = text;
+		}
+
+		if (substitute !== undefined) {
+			result += substitute;
 		} else {
 			result += text.replace(ESCAPE_RE, '\\$1');
 		}
