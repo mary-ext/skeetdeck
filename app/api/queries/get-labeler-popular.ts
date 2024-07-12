f71d16f7a55a4ce9f658366fdaf778d802c008ed
Mary <git@mary.my.id>
2024-07-12 16:32:42 +0700

    chore: update labeler listing

--- app/api/queries/get-labeler-popular.ts
+++ app/api/queries/get-labeler-popular.ts
@@ -9,6 +9,8 @@ const dids: string[] = [
 	'did:plc:hysbs7znfgxyb4tsvetzo4sk', // bskyttrpg.bsky.social
 	'did:plc:aksxl7qy5azlzfm2jstcwqtz', // kiki-bouba.mozzius.dev
 	'did:plc:bpkpvmwpd3nr2ry4btt55ack', // aimod.social
+	'did:plc:yojwcfgpkxq35sv5wioglqad', // perisai.bsky.social
+	'did:plc:wkoofae5uytcm7bjncmev6n6', // pronouns.adorable.mom
 	'did:plc:i65enriuag7n5fgkopbqtkyk', // profile-labels.bossett.social
 	'did:plc:newitj5jo3uel7o4mnf3vj2o', // xblock.aendra.dev
 	'did:plc:z3yk2cflhmn6vmzo3f5ixqh4', // yardcrow.com
@@ -17,28 +19,25 @@ const dids: string[] = [
 	'did:plc:wp7hxfjl5l4zlptn7y6774lk', // baatl.mastod.one
 	'did:plc:jcce2sa3fgue4wiocvf7e7xj', // labeler.flicknow.xyz
 	'did:plc:mjyeurqmqjeexbgigk3yytvb', // nogifsplease.bsky.social
-	'did:plc:wkoofae5uytcm7bjncmev6n6', // pronouns.adorable.mom
 	'did:plc:mpogduvvraozdcbp6w2lafqg', // warlabel.bsky.social
 	'did:plc:5o2g6wwchb3tgwrhl2atauzu', // moderation.tripazeada.club
-	'did:plc:yojwcfgpkxq35sv5wioglqad', // perisai.bsky.social
+	'did:plc:fqfzpua2rp5io5nmxcixvdvm', // oracle.posters.rip
 	'did:plc:5bs7ob2txc2fub2ikvkjgkaf', // labeller.iftas.org
 	'did:plc:3ehw5dwwptcy3xuzugwq2u6t', // stopthebeans.haileyok.com
 	'did:plc:36inn6r2ttwfrt6tpywsjcmt', // phobial.mastod.one
+	'did:plc:ubt73xes4uesthuuhbqwf37d', // labeler.renahlee.com
 	'did:plc:eeptyms6w2crpi6h73ok7qjt', // labeler.shreyanjain.net
 	'did:plc:vnzvtgtwden4hpeierfcfan2', // atproto-spoilers.bsky.social
-	'did:plc:ubt73xes4uesthuuhbqwf37d', // labeler.renahlee.com
 	'did:plc:skibpmllbhxvbvwgtjxl3uao', // anti-aging.bsky.social
 	'did:plc:3eivfiql4memqxkryeu4tqnk', // mod.shawn.party
 	'did:plc:cnn3jrtucivembf66xe6fdfs', // moderation.moe
 	'did:plc:z2i5ah5elywxdcr64i7xai3z', // lavka.bsky.social
+	'did:plc:fro6kbnitc47ia5x23ds4o3b', // arta.mobik.zip
 	'did:plc:fcikraffwejtuqffifeykcml', // ff14labeler.bsky.social
-	'did:plc:fqfzpua2rp5io5nmxcixvdvm', // oracle.posters.rip
 	'did:plc:olmiw2wkm3qoxinal7w5fbnl', // blue-cosmos.bsky.social
 	'did:plc:hwmrwmykflexnxm32pdat5gq', // courtesybot.bsky.social
-	'did:plc:fro6kbnitc47ia5x23ds4o3b', // arta.mobik.zip
-	'did:plc:oz5zavafp7szpd2yyko57ccz', // divylabeler.bsky.social
 	'did:plc:z7wmj3d5t7ytjf4nrh5nq6zx', // lapor.bsky.social
-	'did:plc:gcbmhqcuvuoz7jgmlanabiuv', // label.goeo.lol
+	'did:plc:oz5zavafp7szpd2yyko57ccz', // divylabeler.bsky.social
 	'did:plc:j2zujaxuq33c7nbcqyvgvyvk', // nexus-labeler.bsky.social
 	'did:plc:3ej5knrqezo3meltfsb4vwnv', // tda-labeler.bsky.social
 	'did:plc:nyfwpkztgsrbhrjewnw2p7yo', // labeler.dovgonosyk.xyz
@@ -60,7 +59,9 @@ const dids: string[] = [
 	'did:plc:rprd6z2sgvsdokjsbadytyun', // labels.lio.systems
 	'did:plc:orkjknb6lx4snybzwsolhatt', // labeler.bsky.imax.in.ua
 	'did:plc:l2s5mv6h2j5gyoacsxdxdfom', // labeler.divy.zone
+	'did:plc:3senheyfatcgrq5b2diwxlgs', // marklabeler3.bsky.social
 	'did:plc:3wabolcdllw3w5lnwbrrlp7z', // topical-moderation.bsky.social
+	'did:plc:coynarrmyjsm2s3zkbbr5iow', // yonatanlabel.bsky.social
 	'did:plc:pozgajl56zcftf6nzps46z65', // abandoned293487398.bsky.social
 	'did:plc:jwqm25l6zbkgmorz3k35hf32', // curationkeeper.bsky.social
 	'did:plc:d5dgzxlv5pbpwzkahhdexoce', // gifblock.flop.quest
@@ -74,7 +75,6 @@ const dids: string[] = [
 	'did:plc:lydl7umexooxibfhdtwcijn2', // moderation.veryun.cool
 	'did:plc:mcskx665cnmnkgqnunk6lkrk', // wyvern.blue
 	'did:web:de1.tentacle.expert', // de1.pds.tentacle.expert
-	'did:plc:coynarrmyjsm2s3zkbbr5iow', // yonatanlabel.bsky.social
 ];
 
 export const getLabelerPopularKey = () => {
