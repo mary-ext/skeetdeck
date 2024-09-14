13b8be3a793fcc844225070b5624f78321326c17
Mary <git@mary.my.id>
2024-09-14 11:56:23 +0700

    feat: show videos on gallery view

--- app/com/components/items/GalleryItem.tsx
+++ app/com/components/items/GalleryItem.tsx
@@ -1,25 +1,28 @@
 import { type JSX, createMemo, lazy } from 'solid-js';
 
-import type { AppBskyEmbedImages } from '@atcute/client/lexicons';
+import type { AppBskyEmbedImages, AppBskyEmbedVideo, AppBskyFeedDefs } from '@atcute/client/lexicons';
 
 import { ContextContentMedia, getModerationUI } from '~/api/moderation';
 import { moderatePost } from '~/api/moderation/entities/post';
 import type { SignalizedPost } from '~/api/stores/posts';
 import { getRecordId } from '~/api/utils/misc';
 
+import { openModal } from '~/com/globals/modals';
+import { getModerationOptions } from '~/com/globals/shared';
+
 import { isElementAltClicked, isElementClicked } from '~/utils/interaction';
 import { formatCompact } from '~/utils/intl/number';
 import { clsx } from '~/utils/misc';
 
-import { openModal } from '../../globals/modals';
-import { getModerationOptions } from '../../globals/shared';
-import ChatBubbleIcon from '../../icons/baseline-chat-bubble';
-import CheckboxMultipleBlankIcon from '../../icons/baseline-checkbox-multiple-blank';
-import FavoriteIcon from '../../icons/baseline-favorite';
-import VisibilityIcon from '../../icons/baseline-visibility';
-import { LINK_POST, useLinking } from '../Link';
+import { LINK_POST, useLinking } from '~/com/components/Link';
 
-const ImageViewerDialog = /*#__PURE__*/ lazy(() => import('../dialogs/ImageViewerDialog'));
+import ChatBubbleIcon from '~/com/icons/baseline-chat-bubble';
+import CheckboxMultipleBlankIcon from '~/com/icons/baseline-checkbox-multiple-blank';
+import FavoriteIcon from '~/com/icons/baseline-favorite';
+import VideocamIcon from '~/com/icons/baseline-videocam';
+import VisibilityIcon from '~/com/icons/baseline-visibility';
+
+const ImageViewerDialog = /*#__PURE__*/ lazy(() => import('~/com/components/dialogs/ImageViewerDialog'));
 
 const isDesktop = import.meta.env.VITE_MODE === 'desktop';
 
@@ -34,18 +37,10 @@ const GalleryItem = (props: GalleryItemProps) => {
 		const post = props.post;
 		const embed = post.embed.value;
 
-		let images: AppBskyEmbedImages.ViewImage[] | undefined;
-		if (embed) {
-			if (embed.$type === 'app.bsky.embed.images#view') {
-				images = embed.images;
-			} else if (embed.$type === 'app.bsky.embed.recordWithMedia#view') {
-				if (embed.media.$type === 'app.bsky.embed.images#view') {
-					images = embed.media.images;
-				}
-			}
-		}
+		const image = getPostImage(embed);
+		const video = getPostVideo(embed);
 
-		if (!images) {
+		if ((!image && !video) || (image && video)) {
 			return null;
 		}
 
@@ -55,18 +50,18 @@ const GalleryItem = (props: GalleryItemProps) => {
 			return ui.b.length > 0;
 		});
 
-		const img = images[0];
-		const multiple = images.length > 1;
+		const thumb = image ? image.images[0].thumb : video!.thumbnail;
+		const multiple = image ? image.images.length > 1 : false;
 
 		const handleClick = (ev: MouseEvent | KeyboardEvent) => {
 			if (!isElementClicked(ev)) {
 				return;
 			}
 
-			if (isDesktop && ev.shiftKey) {
+			if (isDesktop && image && ev.shiftKey) {
 				ev.preventDefault();
 
-				openModal(() => <ImageViewerDialog images={images!} active={0} />);
+				openModal(() => <ImageViewerDialog images={/* @once */ image.images} active={0} />);
 				return;
 			}
 
@@ -82,7 +77,7 @@ const GalleryItem = (props: GalleryItemProps) => {
 				onKeyDown={handleClick}
 				class="group relative aspect-square w-full min-w-0 cursor-pointer select-none overflow-hidden bg-muted text-white"
 			>
-				<img src={img.thumb} class={clsx([`h-full w-full object-cover`, shouldBlur() && `scale-110 blur`])} />
+				<img src={thumb} class={clsx([`h-full w-full object-cover`, shouldBlur() && `scale-110 blur`])} />
 
 				{isDesktop && (
 					<div class="invisible absolute inset-0 grid place-items-center bg-black/50 group-hover:visible">
@@ -101,6 +96,7 @@ const GalleryItem = (props: GalleryItemProps) => {
 
 				<div class="absolute left-0 right-0 top-0 m-2 flex items-center justify-end gap-2 text-lg">
 					{shouldBlur() && <VisibilityIcon class="drop-shadow" />}
+					{video && <VideocamIcon class="drop-shadow" />}
 					{multiple && <CheckboxMultipleBlankIcon class="drop-shadow" />}
 				</div>
 			</div>
@@ -109,3 +105,27 @@ const GalleryItem = (props: GalleryItemProps) => {
 };
 
 export default GalleryItem;
+
+const getPostImage = (embed: AppBskyFeedDefs.PostView['embed']): AppBskyEmbedImages.View | undefined => {
+	if (embed) {
+		if (embed.$type === 'app.bsky.embed.images#view') {
+			return embed;
+		}
+
+		if (embed.$type === 'app.bsky.embed.recordWithMedia#view') {
+			return getPostImage(embed.media);
+		}
+	}
+};
+
+const getPostVideo = (embed: AppBskyFeedDefs.PostView['embed']): AppBskyEmbedVideo.View | undefined => {
+	if (embed) {
+		if (embed.$type === 'app.bsky.embed.video#view') {
+			return embed;
+		}
+
+		if (embed.$type === 'app.bsky.embed.recordWithMedia#view') {
+			return getPostVideo(embed.media);
+		}
+	}
+};
